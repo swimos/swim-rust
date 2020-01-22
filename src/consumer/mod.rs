72dca922ffdd4a500dcdae9df62b911756972ba8
Greg Holland <greg.holland@swim.ai>
2020-01-22 11:54:06 +0000

    Added copyright header.

--- src/consumer/mod.rs
+++ src/consumer/mod.rs
@@ -11,14 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-extern crate crossbeam;
-extern crate futures;
-extern crate tokio;
-extern crate tokio_util;
-//extern crate tokio_tungstenite;
-extern crate bytes;
-
-pub mod eff_cell;
-pub mod model;
-pub mod consumer;
