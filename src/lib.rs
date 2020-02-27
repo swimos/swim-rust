027ac161742f53e0fd2c6a84c9760a192d92056b
SirCipher <tk734@york.ac.uk>
2020-02-27 17:41:41 +0000

    Test fail build on warnings

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg_attr(DENY_WARNINGS, deny(warnings))]
+
+
 extern crate crossbeam;
 extern crate futures;
 extern crate futures_util;
