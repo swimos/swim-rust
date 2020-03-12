a2d02aca263b14d228cbc9c6e1fcbbeed5fad885
SirCipher <tk734@york.ac.uk>
2020-03-12 16:33:10 +0000

    Splits up form serializer and deserializer components

---
+++ form/deserialize/src/deserializer.rs
@@ -0,0 +1,395 @@
+// Copyright 2015-2020 SWIM.AI inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::enum_access::Enum;
+use crate::map_access::RecordMap;
+use crate::{DeserializerState, FormDeserializeErr, Result, State, ValueDeserializer};
+use common::model::{Item, Value};
+use serde::de::Visitor;
+use serde::Deserializer;
+
+impl<'de, 'a> Deserializer<'de> for &'a mut ValueDeserializer<'de> {
+    type Error = FormDeserializeErr;
+
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        match self.current_state.value {
+            Some(value) => match value {
+                Value::Record(_attrs, _items) => panic!(),
+                Value::Int32Value(_) => self.deserialize_i32(visitor),
+                Value::Int64Value(_) => self.deserialize_i64(visitor),
+                Value::Extant => self.deserialize_option(visitor),
+                Value::Text(_) => self.deserialize_string(visitor),
+                Value::Float64Value(_) => self.deserialize_f64(visitor),
+                Value::BooleanValue(_) => self.deserialize_bool(visitor),
+            },
+            None => {
+                if let DeserializerState::ReadingAttribute(a) =
+                    self.current_state.deserializer_state
+                {
+                    visitor.visit_string(a.name.to_owned())
+                } else {
+                    Err(FormDeserializeErr::Malformatted)
+                }
+            }
+        }
+    }
+
+    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let Some(Value::BooleanValue(b)) = &self.current_state.value {
+            visitor.visit_bool(*b)
+        } else {
+            Err(FormDeserializeErr::Message(String::from("Unexpected type")))
+        }
+    }
+
+    fn deserialize_i8<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: i8",
+        )))
+    }
+
+    fn deserialize_i16<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: i16",
+        )))
+    }
+
+    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let Some(Value::Int32Value(i)) = &self.current_state.value {
+            visitor.visit_i32(*i)
+        } else {
+            Err(FormDeserializeErr::Message(String::from("Unexpected type")))
+        }
+    }
+
+    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let Some(Value::Int64Value(i)) = &self.current_state.value {
+            visitor.visit_i64(*i)
+        } else {
+            Err(FormDeserializeErr::Message(String::from("Unexpected type")))
+        }
+    }
+
+    fn deserialize_u8<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: u8",
+        )))
+    }
+
+    fn deserialize_u16<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: u16",
+        )))
+    }
+
+    fn deserialize_u32<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: u32",
+        )))
+    }
+
+    fn deserialize_u64<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: u64",
+        )))
+    }
+
+    fn deserialize_f32<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: f32",
+        )))
+    }
+
+    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let Some(Value::Float64Value(f)) = &self.current_state.value {
+            visitor.visit_f64(*f)
+        } else {
+            Err(FormDeserializeErr::Message(String::from("Unexpected type")))
+        }
+    }
+
+    fn deserialize_char<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: char",
+        )))
+    }
+
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let Some(Value::Text(t)) = &self.current_state.value {
+            visitor.visit_borrowed_str(t)
+        } else {
+            Err(FormDeserializeErr::Message(String::from(
+                "Unsupported supported type: str",
+            )))
+        }
+    }
+
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let Some(Value::Text(t)) = &self.current_state.value {
+            visitor.visit_string(t.to_owned())
+        } else {
+            Err(FormDeserializeErr::Message(String::from("Unexpected type")))
+        }
+    }
+
+    fn deserialize_bytes<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: byte",
+        )))
+    }
+
+    fn deserialize_byte_buf<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        Err(FormDeserializeErr::Message(String::from(
+            "Unsupported type: byte",
+        )))
+    }
+
+    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        match self.current_state.value {
+            Some(Value::Extant) => visitor.visit_none(),
+            Some(_value @ Value::Record(_, _)) => visitor.visit_some(self),
+            _ => visitor.visit_some(self),
+        }
+    }
+
+    fn deserialize_unit<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_unit_struct<V>(self, _name: &'static str, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_unit(visitor)
+    }
+
+    fn deserialize_newtype_struct<V>(self, _name: &'static str, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    // Extracts the current value from what is being read.
+    // For records this is the current item (indexed by 'item_index' that has been set by the previous method call.
+    // For tuples, no state has been assigned to the deserializer and so the current value is pushed as a record.
+    // For nested collections, the current value is extracted from the current item that is being deserialized.
+    fn deserialize_seq<V>(mut self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        match &self.current_state.deserializer_state {
+            DeserializerState::ReadingRecord { ref item_index } => match self.current_state.value {
+                Some(Value::Record(_attrs, items)) => {
+                    let value = match items.get(*item_index) {
+                        Some(Item::ValueItem(value)) => value,
+                        Some(Item::Slot(_key, value)) => value,
+                        _ => panic!("Value out of sync with state"),
+                    };
+                    self.push_record(Some(&value));
+                }
+                _ => panic!("Value out of sync with state"),
+            },
+            DeserializerState::ReadingItem(item) => {
+                let value = match item {
+                    Item::ValueItem(value) => value,
+                    Item::Slot(_key, value) => value,
+                };
+                self.push_record(Some(&value));
+            }
+            _ => self.push_record(self.current_state.value),
+        }
+
+        if let Some(Value::Record(_, _)) = self.current_state.value {
+            let result = Ok(visitor.visit_seq(RecordMap::new(&mut self))?);
+            self.pop_state();
+
+            result
+        } else {
+            match self.current_state.value {
+                Some(v) => Err(FormDeserializeErr::IncorrectType(v.to_owned())),
+                None => Err(FormDeserializeErr::Message(String::from("Missing value"))),
+            }
+        }
+    }
+
+    fn deserialize_tuple<V>(self, _len: usize, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_seq(visitor)
+    }
+
+    fn deserialize_tuple_struct<V>(
+        self,
+        _name: &'static str,
+        _len: usize,
+        visitor: V,
+    ) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_seq(visitor)
+    }
+
+    fn deserialize_map<V>(mut self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        let state = {
+            // If we're reading an item then we are reading a nested struct
+            if let DeserializerState::ReadingItem(item) = self.current_state.deserializer_state {
+                match item {
+                    Item::ValueItem(value) => State {
+                        deserializer_state: DeserializerState::ReadingRecord { item_index: 0 },
+                        value: Some(&value),
+                    },
+                    Item::Slot(_key, value) => State {
+                        deserializer_state: DeserializerState::ReadingRecord { item_index: 0 },
+                        value: Some(&value),
+                    },
+                }
+            } else {
+                State {
+                    deserializer_state: DeserializerState::ReadingRecord { item_index: 0 },
+                    value: self.current_state.value,
+                }
+            }
+        };
+
+        self.push_state(state);
+        let value = visitor.visit_map(RecordMap::new(&mut self))?;
+        self.pop_state();
+
+        Ok(value)
+    }
+
+    fn deserialize_struct<V>(
+        self,
+        name: &'static str,
+        _fields: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let DeserializerState::None = &self.current_state.deserializer_state {
+            self.current_state.value = Some(self.input);
+        }
+
+        match &self.current_state.value {
+            Some(value) => {
+                if let Value::Record(attrs, _items) = value {
+                    match attrs.first() {
+                        Some(a) => {
+                            if a.name == name {
+                                self.deserialize_map(visitor)
+                            } else {
+                                Err(FormDeserializeErr::Malformatted)
+                            }
+                        }
+                        None => Err(FormDeserializeErr::Message(String::from("Missing tag"))),
+                    }
+                } else {
+                    unimplemented!()
+                }
+            }
+            None => Err(FormDeserializeErr::Message(String::from("Missing record"))),
+        }
+    }
+
+    fn deserialize_enum<V>(
+        self,
+        _name: &'static str,
+        _variants: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        let value = visitor.visit_enum(Enum::new(self))?;
+        Ok(value)
+    }
+
+    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_any(visitor)
+    }
+
+    fn deserialize_ignored_any<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_any(_visitor)
+    }
+}
---
+++ form/serialize/src/serializer.rs
@@ -0,0 +1,211 @@
+// Copyright 2015-2020 SWIM.AI inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::{FormSerializeErr, Result, SerializerState, ValueSerializer};
+use common::model::{Attr, Value};
+use serde::{Serialize, Serializer};
+
+// CLion/IntelliJ believes there is a missing implementation
+//noinspection RsTraitImplementation
+impl<'a> Serializer for &'a mut ValueSerializer {
+    type Ok = ();
+    type Error = FormSerializeErr;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(self, v: bool) -> Result<()> {
+        self.push_value(Value::from(v));
+        Ok(())
+    }
+
+    fn serialize_i8(self, v: i8) -> Result<()> {
+        self.serialize_i32(i32::from(v))
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<()> {
+        self.serialize_i32(i32::from(v))
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<()> {
+        self.push_value(Value::from(v));
+        Ok(())
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<()> {
+        self.push_value(Value::Int64Value(v));
+        Ok(())
+    }
+
+    fn serialize_u8(self, _v: u8) -> Result<()> {
+        Err(FormSerializeErr::UnsupportedType(String::from("u8")))
+    }
+
+    fn serialize_u16(self, _v: u16) -> Result<()> {
+        Err(FormSerializeErr::UnsupportedType(String::from("u16")))
+    }
+
+    fn serialize_u32(self, _v: u32) -> Result<()> {
+        Err(FormSerializeErr::UnsupportedType(String::from("u32")))
+    }
+
+    fn serialize_u64(self, _v: u64) -> Result<()> {
+        Err(FormSerializeErr::UnsupportedType(String::from("u64")))
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<()> {
+        self.serialize_f64(f64::from(v))
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<()> {
+        self.push_value(Value::Float64Value(v));
+        Ok(())
+    }
+
+    fn serialize_char(self, v: char) -> Result<()> {
+        self.push_value(Value::Text(v.to_string()));
+        Ok(())
+    }
+
+    fn serialize_str(self, v: &str) -> Result<()> {
+        self.push_value(Value::Text(String::from(v)));
+        Ok(())
+    }
+
+    fn serialize_bytes(self, _v: &[u8]) -> Result<()> {
+        Err(FormSerializeErr::UnsupportedType(String::from("u8")))
+    }
+
+    fn serialize_none(self) -> Result<()> {
+        self.push_value(Value::Extant);
+        Ok(())
+    }
+
+    fn serialize_some<T>(self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(self)
+    }
+
+    fn serialize_unit(self) -> Result<()> {
+        self.push_value(Value::of_attr("Unit"));
+        Ok(())
+    }
+
+    fn serialize_unit_struct(self, name: &'static str) -> Result<()> {
+        self.push_attr(Attr::from(name));
+        self.serialize_unit()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+    ) -> Result<()> {
+        self.enter_nested(SerializerState::ReadingNested);
+        self.current_state.serializer_state = SerializerState::ReadingEnumName;
+
+        let result = self.serialize_str(variant);
+        self.current_state.serializer_state = SerializerState::ReadingNested;
+        self.exit_nested();
+
+        result
+    }
+
+    fn serialize_newtype_struct<T>(self, name: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        self.push_attr(Attr::from(name));
+        value.serialize(self)
+    }
+
+    fn serialize_newtype_variant<T>(
+        self,
+        name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        value: &T,
+    ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        self.push_attr(Attr::from(name));
+        self.current_state.attr_name = Some(variant.to_owned());
+        value.serialize(&mut *self)?;
+        Ok(())
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
+        self.enter_nested(SerializerState::ReadingNested);
+        Ok(self)
+    }
+
+    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple> {
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        self.push_attr(Attr::from(name));
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        self.enter_nested(SerializerState::ReadingEnumName);
+        variant.serialize(&mut *self)?;
+        self.current_state.serializer_state = SerializerState::ReadingNested;
+        Ok(self)
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap> {
+        self.enter_nested(SerializerState::ReadingMap(false));
+        Ok(self)
+    }
+
+    fn serialize_struct(self, name: &'static str, _len: usize) -> Result<Self::SerializeStruct> {
+        self.enter_nested(SerializerState::ReadingNested);
+        self.push_attr(Attr::from(name));
+        Ok(self)
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        self.enter_nested(SerializerState::ReadingEnumName);
+        variant.serialize(&mut *self)?;
+        self.current_state.serializer_state = SerializerState::ReadingNested;
+        Ok(self)
+    }
+}
