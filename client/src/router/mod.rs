14e852913f7718c2a3e3dea4eb97bdbd3489589b
DobromirM <mr.d.marinov@gmail.com>
2020-03-25 15:07:51 +0000

    * Refactoring `send` and `receive` message methods of the router.

--- client/src/router/mod.rs
+++ client/src/router/mod.rs
@@ -48,18 +48,18 @@ impl SwimRouter {
         let (router_tx, router_rx) = mpsc::channel(buffer_size);
 
         //TODO add to struct
-        let (connection_request_tx, connection_request_rx) = mpsc::channel(buffer_size);
-        let (messages_tx, messages_rx) = mpsc::channel(buffer_size);
+        // let (connection_request_tx, connection_request_rx) = mpsc::channel(buffer_size);
+        let (envelope_tx, envelope_rx) = mpsc::channel(buffer_size);
 
-        //Todo this is for the sinks
-        let (sinks_tx, sinks_rx) = mpsc::channel(buffer_size);
+        //Todo Use this to create request_sink()
+        // let (sinks_tx, sinks_rx) = mpsc::channel(buffer_size);
 
-        let mut connection_pool =
-            ConnectionPool::new(buffer_size, router_tx, SwimConnectionFactory {});
+        // Todo Use this to create request_connection()
+        // let mut connection_pool =
+        //     ConnectionPool::new(buffer_size, router_tx, SwimConnectionFactory {});
 
-        let receive = SwimRouter::receive_messages_from_pool(router_rx, sinks_rx);
-        let send =
-            SwimRouter::send_messages_to_pool(connection_pool, messages_rx, connection_request_rx);
+        let receive = SwimRouter::receive_messages_from_pool(router_rx);
+        let send = SwimRouter::send_envelopes_to_pool(envelope_rx);
 
         // Todo Add the handlers to the SwimRouter
         let send_handler = tokio::spawn(send);
@@ -71,41 +71,36 @@ impl SwimRouter {
     // rx receives messages directly from every open connection in the pool
     async fn receive_messages_from_pool(
         mut router_rx: mpsc::Receiver<Result<ConnectionPoolMessage, ConnectionError>>,
-        mut sinks_rx: mpsc::Receiver<SenderErrInto<mpsc::Sender<Envelope>, RoutingError>>,
     ) {
         loop {
-            let message = router_rx.recv().await.unwrap().unwrap();
+            let pool_message = router_rx.recv().await.unwrap().unwrap();
+            let ConnectionPoolMessage { host, message } = pool_message;
 
-            //TODO sink should be selected based on message host
-            let mut sink = sinks_rx.recv().await.unwrap();
+            //TODO this needs to be implemented
+            let mut sink = SwimRouter::request_sink(host);
+            sink.send_item(text);
 
-            //TODO parse the message to envelope.
-            let lane_addressed = LaneAddressed {
-                node_uri: String::from("node_uri"),
-                lane_uri: String::from("lane_uri"),
-                body: None,
-            };
-            let envelope = Envelope::EventMessage(lane_addressed);
+            //TODO parse the message to envelope. This should be moved in the subtasks.
 
-            sink.send_item(envelope);
+            // let lane_addressed = LaneAddressed {
+            //     node_uri: String::from("node_uri"),
+            //     lane_uri: String::from("lane_uri"),
+            //     body: None,
+            // };
+            // let envelope = Envelope::EventMessage(lane_addressed);
+            //
+            // sink.send_item(envelope);
         }
     }
 
-    async fn send_messages_to_pool(
-        mut connection_pool: ConnectionPool,
-        mut message_rx: mpsc::Receiver<String>,
-        mut connection_request_rx: mpsc::Receiver<url::Url>,
-    ) {
+    async fn send_envelopes_to_pool(mut envelope_rx: mpsc::Receiver<Envelope>) {
         //Todo wrap message and host into one struct
         loop {
-            let message = message_rx.recv().await.unwrap();
-            let connection_host = connection_request_rx.recv().await.unwrap();
-            let mut connection = connection_pool
-                .request_connection(connection_host)
-                .unwrap()
-                .await
-                .unwrap()
-                .unwrap();
+            let envelope = envelope_rx.recv().await.unwrap();
+            //TODO Parse the envelope to obtain host
+            let host = String::from("foo");
+            let mut connection = SwimRouter::request_connection(host);
+
             connection.send_message(&message);
         }
     }
