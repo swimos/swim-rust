5d705c1636184e56443e73d3e6d0c72b8595a17f
Greg Holland <greg.holland@swim.ai>
2020-05-11 17:00:19 +0100

    Direct constructors for incoming/outgoing messages.

--- client/src/downlink/subscription/envelopes/tests.rs
+++ client/src/downlink/subscription/envelopes/tests.rs
@@ -19,7 +19,7 @@ use hamcrest2::prelude::*;
 use super::*;
 use crate::downlink::Message;
 use common::model::Value::Int32Value;
-use common::warp::envelope::Envelope;
+use common::warp::envelope::LinkMessage;
 
 fn path() -> AbsolutePath {
     AbsolutePath::new("host", "node", "lane")
@@ -27,7 +27,7 @@ fn path() -> AbsolutePath {
 
 #[test]
 fn unlink_value_command_to_envelope() {
-    let expected = Envelope::unlink("node", "lane").into_outgoing().unwrap();
+    let expected = LinkMessage::unlink("node", "lane");
     let (host, envelope) = value_envelope(&path(), Command::Unlink);
     assert_that!(host, eq("host"));
     assert_that!(envelope, eq(expected));
@@ -35,14 +35,14 @@ fn unlink_value_command_to_envelope() {
 
 #[test]
 fn unlinked_value_message_from_envelope() {
-    let env = Envelope::unlinked("node", "lane").into_incoming().unwrap();
+    let env = LinkMessage::unlinked("node", "lane");
     let result = value::from_envelope(env);
     assert_that!(result, eq(Message::Unlinked));
 }
 
 #[test]
 fn sync_value_command_to_envelope() {
-    let expected = Envelope::sync("node", "lane").into_outgoing().unwrap();
+    let expected = LinkMessage::sync("node", "lane");
     let (host, envelope) = value_envelope(&path(), Command::Sync);
     assert_that!(host, eq("host"));
     assert_that!(envelope, eq(expected));
@@ -50,23 +50,21 @@ fn sync_value_command_to_envelope() {
 
 #[test]
 fn linked_value_message_from_envelope() {
-    let env = Envelope::linked("node", "lane").into_incoming().unwrap();
+    let env = LinkMessage::linked("node", "lane");
     let result = value::from_envelope(env);
     assert_that!(result, eq(Message::Linked));
 }
 
 #[test]
 fn synced_value_message_from_envelope() {
-    let env = Envelope::synced("node", "lane").into_incoming().unwrap();
+    let env = LinkMessage::synced("node", "lane");
     let result = value::from_envelope(env);
     assert_that!(result, eq(Message::Synced));
 }
 
 #[test]
 fn data_value_command_to_envelope() {
-    let expected = Envelope::make_command("node", "lane", Some(Int32Value(5)))
-        .into_outgoing()
-        .unwrap();
+    let expected = LinkMessage::make_command("node", "lane", Some(Int32Value(5)));
     let (host, envelope) = value_envelope(
         &path(),
         Command::Action(SharedValue::new(Value::Int32Value(5))),
@@ -77,16 +75,14 @@ fn data_value_command_to_envelope() {
 
 #[test]
 fn data_value_message_from_envelope() {
-    let env = Envelope::make_event("node", "lane", Some(Int32Value(7)))
-        .into_incoming()
-        .unwrap();
+    let env = LinkMessage::make_event("node", "lane", Some(Int32Value(7)));
     let result = value::from_envelope(env);
     assert_that!(result, eq(Message::Action(Int32Value(7))))
 }
 
 #[test]
 fn unlink_map_command_to_envelope() {
-    let expected = Envelope::unlink("node", "lane").into_outgoing().unwrap();
+    let expected = LinkMessage::unlink("node", "lane");
     let (host, envelope) = map_envelope(&path(), Command::Unlink);
     assert_that!(host, eq("host"));
     assert_that!(envelope, eq(expected));
@@ -94,7 +90,7 @@ fn unlink_map_command_to_envelope() {
 
 #[test]
 fn sync_map_command_to_envelope() {
-    let expected = Envelope::sync("node", "lane").into_outgoing().unwrap();
+    let expected = LinkMessage::sync("node", "lane");
     let (host, envelope) = map_envelope(&path(), Command::Sync);
     assert_that!(host, eq("host"));
     assert_that!(envelope, eq(expected));
@@ -104,9 +100,7 @@ fn sync_map_command_to_envelope() {
 fn clear_map_command_to_envelope() {
     let rep = Form::into_value(MapModification::Clear);
 
-    let expected = Envelope::make_command("node", "lane", Some(rep))
-        .into_outgoing()
-        .unwrap();
+    let expected = LinkMessage::make_command("node", "lane", Some(rep));
     let (host, envelope) = map_envelope(&path(), Command::Action(MapModification::Clear));
     assert_that!(host, eq("host"));
     assert_that!(envelope, eq(expected));
@@ -116,9 +110,7 @@ fn clear_map_command_to_envelope() {
 fn take_map_command_to_envelope() {
     let rep = Form::into_value(MapModification::Take(7));
 
-    let expected = Envelope::make_command("node", "lane", Some(rep))
-        .into_outgoing()
-        .unwrap();
+    let expected = LinkMessage::make_command("node", "lane", Some(rep));
     let (host, envelope) = map_envelope(&path(), Command::Action(MapModification::Take(7)));
     assert_that!(host, eq("host"));
     assert_that!(envelope, eq(expected));
@@ -128,9 +120,7 @@ fn take_map_command_to_envelope() {
 fn skip_map_command_to_envelope() {
     let rep = Form::into_value(MapModification::Skip(7));
 
-    let expected = Envelope::make_command("node", "lane", Some(rep))
-        .into_outgoing()
-        .unwrap();
+    let expected = LinkMessage::make_command("node", "lane", Some(rep));
     let (host, envelope) = map_envelope(&path(), Command::Action(MapModification::Skip(7)));
     assert_that!(host, eq("host"));
     assert_that!(envelope, eq(expected));
@@ -142,9 +132,7 @@ fn remove_map_command_to_envelope() {
 
     let rep = Form::as_value(&action);
 
-    let expected = Envelope::make_command("node", "lane", Some(rep))
-        .into_outgoing()
-        .unwrap();
+    let expected = LinkMessage::make_command("node", "lane", Some(rep));
     let (host, envelope) = map_envelope(
         &path(),
         Command::Action(MapModification::Remove(Value::text("key"))),
@@ -159,9 +147,7 @@ fn insert_map_command_to_envelope() {
 
     let rep = Form::as_value(&action);
 
-    let expected = Envelope::make_command("node", "lane", Some(rep))
-        .into_outgoing()
-        .unwrap();
+    let expected = LinkMessage::make_command("node", "lane", Some(rep));
 
     let arc_action = MapModification::Insert(Value::text("key"), Arc::new(Value::text("value")));
 
@@ -172,21 +158,21 @@ fn insert_map_command_to_envelope() {
 
 #[test]
 fn unlinked_map_message_from_envelope() {
-    let env = Envelope::unlinked("node", "lane").into_incoming().unwrap();
+    let env = LinkMessage::unlinked("node", "lane");
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Unlinked));
 }
 
 #[test]
 fn linked_map_message_from_envelope() {
-    let env = Envelope::linked("node", "lane").into_incoming().unwrap();
+    let env = LinkMessage::linked("node", "lane");
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Linked));
 }
 
 #[test]
 fn synced_map_message_from_envelope() {
-    let env = Envelope::synced("node", "lane").into_incoming().unwrap();
+    let env = LinkMessage::synced("node", "lane");
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Synced));
 }
@@ -194,9 +180,7 @@ fn synced_map_message_from_envelope() {
 #[test]
 fn clear_map_message_from_envelope() {
     let rep = Form::into_value(MapModification::Clear);
-    let env = Envelope::make_event("node", "lane", Some(rep))
-        .into_incoming()
-        .unwrap();
+    let env = LinkMessage::make_event("node", "lane", Some(rep));
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Action(MapModification::Clear)))
 }
@@ -204,9 +188,7 @@ fn clear_map_message_from_envelope() {
 #[test]
 fn take_map_message_from_envelope() {
     let rep = Form::into_value(MapModification::Take(14));
-    let env = Envelope::make_event("node", "lane", Some(rep))
-        .into_incoming()
-        .unwrap();
+    let env = LinkMessage::make_event("node", "lane", Some(rep));
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Action(MapModification::Take(14))))
 }
@@ -214,9 +196,7 @@ fn take_map_message_from_envelope() {
 #[test]
 fn skip_map_message_from_envelope() {
     let rep = Form::into_value(MapModification::Skip(1));
-    let env = Envelope::make_event("node", "lane", Some(rep))
-        .into_incoming()
-        .unwrap();
+    let env = LinkMessage::make_event("node", "lane", Some(rep));
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Action(MapModification::Skip(1))))
 }
@@ -226,9 +206,7 @@ fn remove_map_message_from_envelope() {
     let action = MapModification::Remove(Value::text("key"));
 
     let rep = Form::as_value(&action);
-    let env = Envelope::make_event("node", "lane", Some(rep))
-        .into_incoming()
-        .unwrap();
+    let env = LinkMessage::make_event("node", "lane", Some(rep));
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Action(action)))
 }
@@ -238,9 +216,7 @@ fn insert_map_message_from_envelope() {
     let action = MapModification::Insert(Value::text("key"), Value::text("value"));
 
     let rep = Form::as_value(&action);
-    let env = Envelope::make_event("node", "lane", Some(rep))
-        .into_incoming()
-        .unwrap();
+    let env = LinkMessage::make_event("node", "lane", Some(rep));
     let result = map::from_envelope(env);
     assert_that!(result, eq(Message::Action(action)))
 }
