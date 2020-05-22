9f84273e626d4670ca4a12174795509240569823
Greg Holland <greg.holland@swim.ai>
2020-05-22 10:18:49 +0100

    Basics of transactional variables.

--- stm/src/lib.rs
+++ stm/src/lib.rs
@@ -13,3 +13,4 @@
 // limitations under the License.
 
 pub(crate) mod ptr;
+pub mod var;
---
+++ stm/src/var/mod.rs
@@ -0,0 +1,93 @@
+// Copyright 2015-2020 SWIM.AI inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use futures::task::AtomicWaker;
+use tokio::sync::RwLock;
+use std::sync::Arc;
+use std::fmt::{Debug, Formatter};
+
+pub(crate) struct TVarInner<T> {
+    content: RwLock<Arc<T>>,
+    waker: AtomicWaker,
+}
+
+impl<T: Send + Sync + 'static> TVarInner<T> {
+
+    pub(crate) fn new(value: T) -> Self {
+        TVarInner {
+            content: RwLock::new(Arc::new(value)),
+            waker: AtomicWaker::new()
+        }
+    }
+
+}
+
+
+
+#[derive(Clone)]
+pub struct TVar<T>(Arc<TVarInner<T>>);
+
+impl<T> Debug for TVar<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "TVar<{}>", std::any::type_name::<T>())
+    }
+}
+
+pub struct TVarRead<T>(pub(crate) Arc<TVarInner<T>>);
+
+impl<T> Debug for TVarRead<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Read[TVar<{}>]", std::any::type_name::<T>())
+    }
+}
+
+pub struct TVarWrite<T> {
+    pub(crate) inner: Arc<TVarInner<T>>,
+    pub(crate) value: T,
+}
+
+impl<T: Debug> Debug for TVarWrite<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Write[TVar<{}> << {:?}]", std::any::type_name::<T>(), &self.value)
+    }
+}
+
+impl<T: Send + Sync + 'static> TVar<T> {
+
+    pub fn new(initial: T) -> Self {
+        TVar(Arc::new(TVarInner::new(initial)))
+    }
+
+}
+
+impl<T> TVar<T> {
+
+    pub fn get(&self) -> TVarRead<T> {
+        let TVar(inner) = self;
+        TVarRead(inner.clone())
+    }
+
+    pub fn put(&self, value: T) -> TVarWrite<T> {
+        let TVar(inner) = self;
+        TVarWrite {
+            inner: inner.clone(),
+            value
+        }
+    }
+
+    pub fn same_var(&self, other: &Self) -> bool {
+        Arc::ptr_eq(&self.0, &other.0)
+    }
+
+}
