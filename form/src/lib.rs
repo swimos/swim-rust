975d2aa36e73a54d4d9ed9e9ebf2d36678735b8d
Greg Holland <greg.holland@swim.ai>
2020-06-04 13:52:15 +0100

    Adds Clippy exception to all crates.

--- form/src/lib.rs
+++ form/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::match_wild_err_arm)]
+
 pub extern crate common as _common;
 pub extern crate deserialize as _deserialize;
 #[macro_use]
