fabaec3d1ccd3f98704849c116d68da20e5d67c7
DobromirM <mr.d.marinov@gmail.com>
2020-06-10 11:51:26 +0100

    * Removed useless test.

--- client/src/downlink/model/command/tests.rs
+++ client/src/downlink/model/command/tests.rs
@@ -10,28 +10,6 @@ use form::ValidatedForm;
 use tokio::sync::mpsc;
 use tokio::time::Duration;
 
-#[tokio::test]
-async fn test_create_command_downlink() {
-    let (tx, mut rx) = mpsc::channel(5);
-    let params = DownlinkParams::new_buffered(
-        BackpressureMode::Propagate,
-        5,
-        Duration::from_micros(100),
-        5,
-        OnInvalidMessage::Terminate,
-        256,
-    )
-    .unwrap();
-
-    command::create_downlink(
-        StandardSchema::Anything,
-        tx.map_err_into::<RoutingError>(),
-        &params,
-    );
-
-    assert_eq!(rx.recv().await.unwrap(), Command::Sync);
-}
-
 #[test]
 fn test_handle_action_valid() {
     let machine = CommandStateMachine::new(i32::schema());
