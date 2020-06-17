4fba191c9e70a1edac3652aa1d8e27fa8169cf40
SirCipher <tklapwijk92@gmail.com>
2020-06-17 16:56:32 +0100

    Adds macro to check trait objects for equality.

---
+++ utilities/src/ptr/mod.rs
@@ -0,0 +1,84 @@
+// Copyright 2015-2020 SWIM.AI inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// Tests two trait objects for equality. This is useful when dealing with boxed trait objects and
+/// equality tests are still required.
+///
+/// Example:
+/// ```
+/// use std::sync::Arc;
+/// use self::utilities::trait_eq;
+///
+/// trait TestTrait {}
+///
+/// struct S;
+/// impl TestTrait for S {}
+///
+/// #[derive(Clone)]
+/// pub struct Sender {
+///     task: Arc<dyn TestTrait>,
+/// }
+///
+/// let s1 = Sender { task: Arc::new(S) };
+/// let s2 = s1.clone();
+///
+/// assert!(trait_eq!(&*s1.task, &*s2.task, TestTrait));
+/// ```
+#[macro_export]
+macro_rules! trait_eq {
+    ($left:expr, $right:expr, $tr:ident) => {{
+        let l = $left as &dyn $tr as *const dyn $tr as *const u8;
+        let r = $right as &dyn $tr as *const dyn $tr as *const u8;
+
+        std::ptr::eq(l, r)
+    }};
+}
+
+#[cfg(test)]
+mod trait_eq_tests {
+    use std::sync::Arc;
+
+    trait TestTrait {}
+
+    #[test]
+    fn eq() {
+        #[derive(Clone)]
+        pub struct Sender {
+            task: Arc<dyn TestTrait>,
+        }
+
+        struct S;
+
+        impl TestTrait for S {}
+
+        let s1 = Sender { task: Arc::new(S) };
+        let s2 = s1.clone();
+
+        assert!(trait_eq!(&*s1.task, &*s2.task, TestTrait));
+    }
+
+    #[test]
+    fn not_eq() {
+        struct A;
+        struct B;
+
+        impl TestTrait for A {}
+        impl TestTrait for B {}
+
+        let a = A;
+        let b = B;
+
+        assert!(!trait_eq!(&a, &b, TestTrait));
+    }
+}
