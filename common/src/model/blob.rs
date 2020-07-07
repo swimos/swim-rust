866ba293d42b594953939e8c78d6c04c1e858f3c
SirCipher <tklapwijk92@gmail.com>
2020-07-07 16:27:00 +0100

    Adds blob support to the form deserializer

--- common/src/model/blob.rs
+++ common/src/model/blob.rs
@@ -123,7 +123,7 @@ impl Write for Blob {
         self.data.flush()
     }
 
-    fn write_all(&mut self, mut buf: &[u8]) -> io::Result<()> {
+    fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
         self.data.write_all(buf)
     }
 }
