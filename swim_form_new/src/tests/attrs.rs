e0d17f627b1be74da31040825b1721bea3d1dfac
SirCipher <tklapwijk92@gmail.com>
2020-07-14 14:11:17 +0100

    Adds unit tests for serialization field renaming

--- swim_form_new/src/tests/attrs.rs
+++ swim_form_new/src/tests/attrs.rs
@@ -13,14 +13,14 @@
 // limitations under the License.
 
 use crate::Form;
-use common::model::Value;
+use common::model::{Attr, Item, Value};
 
 mod swim_form {
     pub use crate::*;
 }
 
 #[test]
-fn test_rename() {
+fn test_rename_single() {
     #[form(Value)]
     struct F {
         #[form(ser_name = "epoch")]
@@ -29,5 +29,69 @@ fn test_rename() {
 
     let fs = F { age: 30 };
     let value: Value = fs.as_value();
-    println!("{}", value);
+    let expected = Value::Record(vec![Attr::of("F")], vec![Item::slot("epoch", 30)]);
+
+    assert_eq!(value, expected);
+}
+
+#[test]
+fn test_rename_multiple() {
+    #[form(Value)]
+    struct F {
+        #[form(ser_name = "1")]
+        a: i32,
+        #[form(ser_name = "2")]
+        b: i32,
+        #[form(ser_name = "3")]
+        c: i32,
+        #[form(ser_name = "4")]
+        d: i32,
+        #[form(ser_name = "5")]
+        e: i32,
+    }
+
+    let fs = F {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+        e: 4,
+    };
+
+    let value: Value = fs.as_value();
+    let expected = Value::Record(
+        vec![Attr::of("F")],
+        vec![
+            Item::slot("1", 0),
+            Item::slot("2", 1),
+            Item::slot("3", 2),
+            Item::slot("4", 3),
+            Item::slot("5", 4),
+        ],
+    );
+
+    assert_eq!(value, expected);
+}
+
+#[test]
+fn rename_duplicates() {
+    #[form(Value)]
+    struct F {
+        #[form(ser_name = "epoch")]
+        age: i32,
+        #[form(ser_name = "epoch")]
+        name: String,
+    }
+
+    let fs = F {
+        age: 30,
+        name: String::from("swim"),
+    };
+    let value: Value = fs.as_value();
+    let expected = Value::Record(
+        vec![Attr::of("F")],
+        vec![Item::slot("epoch", 30), Item::slot("epoch", "swim")],
+    );
+
+    assert_eq!(value, expected);
 }
