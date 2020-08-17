7fc3b3264c43d6ff10fd9c2598c5e1abd2497ab1
SirCipher <tklapwijk92@gmail.com>
2020-08-17 20:42:56 +0100

    Adds some ValidatedForm derive documentation

--- swim_common/src/form/mod.rs
+++ swim_common/src/form/mod.rs
@@ -332,6 +332,327 @@ pub trait Form: Sized {
 
 /// A [`Form`] with an associated schema that can validate [`Value`] instances without attempting
 /// to convert them.
+///
+/// See [`StandardSchema`] for details of schema variants.
+///
+/// # How can I implement `ValidatedForm`?
+/// # Structures
+/// Given the following `struct` and its expected value:
+///
+/// ```
+/// use swim_common::model::{Attr, Item, Value};
+/// use swim_common::form::Form;
+///
+/// #[derive(Form)]
+/// struct Cat {
+///     name: String,
+/// }
+///
+/// let cat = Cat {
+///     name: String::from("Dill"),
+/// };
+///
+/// assert_eq!(cat.as_value(), Value::Record(
+///     vec![Attr::of("Cat")],
+///     vec![Item::Slot(Value::text("name"), Value::text("Dill"))]
+/// ));
+/// ```
+///
+/// We can observe that the schema for this `Record` is a single attribute with an `Extant` body
+/// and a single `Slot` with a key of "name" and a String value The expected schema for this
+/// is:
+/// ```
+/// use swim_common::model::{Attr, Item, Value, ValueKind};
+/// use swim_common::form::{Form, ValidatedForm};
+/// use swim_common::model::schema::{StandardSchema, ItemSchema, Schema};
+/// use swim_common::model::schema::attr::AttrSchema;
+/// use swim_common::model::schema::slot::SlotSchema;
+///
+/// #[derive(Form)]
+/// struct Cat {
+///     name: String,
+/// }
+///
+/// impl ValidatedForm for Cat {
+///     fn schema() -> StandardSchema {
+///        StandardSchema::HeadAttribute {
+///            schema: Box::new(AttrSchema::named(
+///                "Cat",
+///                StandardSchema::OfKind(ValueKind::Extant),
+///            )),
+///            required: true,
+///            remainder: Box::new(StandardSchema::Layout {
+///                items: vec![(
+///                    ItemSchema::Field(SlotSchema::new(
+///                        StandardSchema::text("name"),
+///                        String::schema(),
+///                    )),
+///                    true,
+///                )],
+///                exhaustive: true,
+///            }),
+///        }
+///     }
+/// }
+///
+/// let cat = Cat {
+///     name: String::from("Dill"),
+/// }
+/// .as_value();
+///
+/// assert!(Cat::schema().matches(&cat));
+/// ```
+///
+/// # Enumerations
+/// Implementing `ValidatedForm` for enumerations works the same way as structures except that the
+/// variants are logically OR'd together as a schema.
+/// ```
+/// use swim_common::model::{Attr, Item, Value, ValueKind};
+/// use swim_common::form::{Form, ValidatedForm};
+/// use swim_common::model::schema::{StandardSchema, ItemSchema, Schema};
+/// use swim_common::model::schema::attr::AttrSchema;
+/// use swim_common::model::schema::slot::SlotSchema;
+///
+/// #[derive(Form)]
+/// enum Food {
+///     Kebab,
+///     Pizza,
+///     Pasta,
+/// }
+///
+/// impl ValidatedForm for Food {
+///     fn schema() -> StandardSchema {
+///         StandardSchema::Or(vec![
+///             StandardSchema::HeadAttribute {
+///                 schema: Box::new(AttrSchema::named(
+///                     "Kebab",
+///                     StandardSchema::OfKind(ValueKind::Extant),
+///                 )),
+///                 required: false,
+///                 remainder: Box::new(StandardSchema::Layout {
+///                     items: vec![],
+///                     exhaustive: false,
+///                 }),
+///             },
+///             StandardSchema::HeadAttribute {
+///                 schema: Box::new(AttrSchema::named(
+///                     "Pizza",
+///                     StandardSchema::OfKind(ValueKind::Extant),
+///                 )),
+///                 required: false,
+///                 remainder: Box::new(StandardSchema::Layout {
+///                     items: vec![],
+///                     exhaustive: false,
+///                 }),
+///             },
+///             StandardSchema::HeadAttribute {
+///                 schema: Box::new(AttrSchema::named(
+///                     "Pasta",
+///                     StandardSchema::OfKind(ValueKind::Extant),
+///                 )),
+///                 required: false,
+///                 remainder: Box::new(StandardSchema::Layout {
+///                     items: vec![],
+///                     exhaustive: false,
+///                 }),
+///             },
+///         ])
+///     }
+/// }
+///
+/// let food = Food::Kebab.as_value();
+/// assert!(Food::schema().matches(&food));
+/// ```
+///
+/// # Deriving `ValidatedForm`
+/// The `ValidatedForm` trait can also be used with `#[derive]` if all of the fields implement
+/// `ValidatedForm` and the implementor also implements `Form` (as required by the `ValidatedForm`
+/// trait).
+///
+/// The derive macro supports all of the `StandardSchema` variants in a Snake Case format except
+/// for: `HeadAttribute`, `HasAttributes`, `HasSlots` and `Layout`. Implementors which require these
+/// variants will have to be implemented manually.
+///
+/// ## Usage
+/// Any `struct` or `enum` annotated with `#[derive(ValidatedForm)]` will have a schema derived for
+/// it based on its fields. Fields not attributed with `#[form(schema(..)]` will use the type's
+/// implementation of `ValidatedForm`.
+///
+/// ```
+/// use swim_common::model::{Attr, Item, Value, ValueKind};
+/// use swim_common::form::{Form, ValidatedForm};
+/// use swim_common::model::schema::{StandardSchema, ItemSchema, Schema};
+/// use swim_common::model::schema::attr::AttrSchema;
+/// use swim_common::model::schema::slot::SlotSchema;
+///
+/// #[derive(Form, ValidatedForm)]
+/// #[form(tag = "Structure")]
+/// struct S {
+///     a: i32,
+///     b: i64,
+/// }
+///
+/// let expected_schema = StandardSchema::HeadAttribute {
+///     schema: Box::new(AttrSchema::named(
+///         "Structure",
+///         StandardSchema::OfKind(ValueKind::Extant),
+///     )),
+///     required: true,
+///     remainder: Box::new(StandardSchema::Layout {
+///         items: vec![
+///             (
+///                 ItemSchema::Field(SlotSchema::new(StandardSchema::text("a"), i32::schema())),
+///                 true,
+///             ),
+///             (
+///                 ItemSchema::Field(SlotSchema::new(StandardSchema::text("b"), i64::schema())),
+///                 true,
+///             ),
+///         ],
+///         exhaustive: true,
+///     }),
+/// };
+///
+/// assert_eq!(S::schema(), expected_schema);
+/// ```
+///
+/// ## Attributes
+/// The `ValidatedForm` macro supports attributes in the path of `#[form(schema(..))]`.
+///
+/// The derivo macro supports all the attributes that the `Form` derive macro supports. With the
+/// exception of any field marked as `#[form(skip)]` may not also contain a schema.
+///
+/// Similar to the `Form` derive, `ValidatedForm` derivation supports attributes at `struct`,
+/// variant, and field level placement. Detail on attributes that support this is listed below.
+///
+/// ### anything
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+///
+/// #[derive(Form, ValidatedForm)]
+/// #[form(schema(anything))]
+/// struct S {
+///     a: i32,
+///     b: i64,
+/// }
+/// ```
+/// The `anything` attribute is supported at a structure, variant and field level. When placed at
+/// a structure or variant level, the derived schema is `StandardSchema::Anything`. At a field
+/// level, the value of the field is written as `StandardSchema::Anything`.
+///
+/// ### nothing
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+///
+/// #[derive(Form, ValidatedForm)]
+/// #[form(schema(nothing))]
+/// struct S {
+///     a: i32,
+///     b: i64,
+/// }
+/// ```
+/// The `nothing` attribute is supported at a structure, variant and field level. When placed at
+/// a structure or variant level, the derived schema is `StandardSchema::Nothing`. At a field
+/// level, the value of the field is written as `StandardSchema::Nothing`.
+///
+/// ### num_attrs
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+/// use swim_common::model::Value;
+///
+/// #[derive(Form, ValidatedForm)]
+/// struct S {
+///     #[form(schema(num_attrs = 5))]
+///     value: Value
+/// }
+/// ```
+/// Asserts that a `Value` has the provided number of attributes.
+///
+/// ### num_items
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+/// use swim_common::model::Value;
+///
+/// #[derive(Form, ValidatedForm)]
+/// struct S {
+///     #[form(schema(num_items = 5))]
+///     value: Value
+/// }
+/// ```
+/// Asserts that a `Value` has the provided number of items.
+///
+/// ### of_kind
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+/// use swim_common::model::{Value, ValueKind};
+///
+/// #[derive(Form, ValidatedForm)]
+/// struct S {
+///     #[form(schema(of_kind(ValueKind::Int32)))]
+///     value: Value
+/// }
+/// ```
+/// Asserts that a `Value` is of the provided `ValueKind`
+///
+/// ### equal
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+/// use swim_common::model::Value;
+///
+/// fn equals_i32_max() -> Value {
+///     Value::Int32Value(i32::max_value())
+/// }
+///
+/// #[derive(Form, ValidatedForm)]
+/// struct S {
+///     #[form(schema(equal = "equals_i32_max"))]
+///     value: i32
+/// }
+/// ```
+/// Valid at structure, variant and field level, asserts that a value matches the provided value.
+/// This attribute accepts a function identity in the format of `fn() -> Value`.
+///
+/// ### text
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+///
+/// #[derive(Form, ValidatedForm)]
+/// struct S {
+///     #[form(schema(text = "Swim"))]
+///     string: String
+/// }
+/// ```
+/// Builds a `TextSchema` and asserts that a value matches it.
+///
+/// ### non_nan
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+///
+/// #[derive(Form, ValidatedForm)]
+/// struct S {
+///     #[form(schema(non_nan))]
+///     f: f64
+/// }
+/// ```
+/// Asserts that a [`Value`] is a non-NaN floating point number.
+///
+/// ### finite
+/// ```
+/// use swim_common::form::{Form, ValidatedForm};
+///
+/// #[derive(Form, ValidatedForm)]
+/// struct S {
+///     #[form(schema(finite))]
+///     f: f64
+/// }
+/// ```
+/// Asserts that a [`Value`] is a finite floating point number.
+///
+/// ### Ranges
+/// ### all_items
+/// ### and
+/// ### or
+/// ### not
 pub trait ValidatedForm: Form {
     /// A schema for the form. If the schema returns true for a [`Value`] the form should be able
     /// to create an instance of the type from the [`Value`] without generating an error.
