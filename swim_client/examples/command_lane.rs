3aa37e1d41274ee2e249ef86e80fdf228ed014d3
SirCipher <tklapwijk92@gmail.com>
2020-12-15 13:30:15 +0000

    Moves cookbooks to be client examples

--- swim_client/examples/command_lane.rs
+++ swim_client/examples/command_lane.rs
@@ -13,9 +13,11 @@
 // limitations under the License.
 
 use std::time::Duration;
+use swim_client::downlink::typed::SchemaViolations;
 use swim_client::interface::SwimClient;
-use swim_common::model::Value;
 use swim_common::warp::path::AbsolutePath;
+use swim_runtime::time::delay::delay_for;
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
@@ -24,17 +26,30 @@ async fn main() {
     let node_uri = "unit/foo";
     let lane_uri = "publish";
 
-    let path = AbsolutePath::new(host_uri.clone(), node_uri, lane_uri);
+    let path = AbsolutePath::new(host_uri, node_uri, lane_uri);
 
-    for _ in 0..10 {
-        client
-            .send_command(path.clone(), Value::Extant)
-            .await
-            .expect("Failed to send command!");
+    let mut event_dl = client
+        .event_downlink::<i64>(path.clone(), SchemaViolations::Ignore)
+        .await
+        .unwrap();
 
-        tokio::time::delay_for(Duration::from_secs(5)).await;
-    }
+    task::spawn(async move {
+        while let Some(event) = event_dl.recv().await {
+            println!("Link received event: {}", event)
+        }
+    });
+
+    // command() `msg` TO
+    // the "publish" lane OF
+    // the agent addressable by `/unit/foo` RUNNING ON
+    // the plane with hostUri "warp://localhost:9001"
+    let msg = 9035768;
+    client
+        .send_command(path, msg)
+        .await
+        .expect("Failed to send a command!");
+    delay_for(Duration::from_secs(2)).await;
 
     println!("Stopping client in 2 seconds");
-    tokio::time::delay_for(Duration::from_secs(2)).await;
+    delay_for(Duration::from_secs(2)).await;
 }
