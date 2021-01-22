f6ac534cd5b6c241e05e0d2cf4fb523b607551c1
SirCipher <tklapwijk92@gmail.com>
2021-01-22 09:12:44 +0000

    Adds more lane types to the swim_agent macro

--- swim_server/src/macros.rs
+++ swim_server/src/macros.rs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::agent::lane::lifecycle::{LaneLifecycle, StatefulLaneLifecycleBase};
-use crate::agent::lane::model::map::MapLane;
-use crate::agent::lane::model::value::ValueLane;
 use crate::agent::lane::strategy::Queue;
 
 mod swim_server {
@@ -23,6 +20,7 @@ mod swim_server {
 
 struct SwimAgentConfig;
 
+// todo: impl agent get_lanes to be able to build a server?
 macro_rules! swim_agent {
     // entry: agent with no lanes
     ( ($name: ident, $config: ident) => { } ) => {
@@ -35,13 +33,13 @@ macro_rules! swim_agent {
     // value lane
     (@ { $name: ident, $config: ident, [$($element: ident: $ty: ty)*] [$($out_impl:tt)*] }
         lane(vis: $visibility: ty, path: $path: tt, $lane_name:ident : ValueLane<$key: ident>, lifecycle: $lifecycle:ident, watch: $watch_type:ty) => {
-            on_start($on_start_model:ident, $on_start_context:ident) => $on_start:block
-            on_event($on_event_event:ident, $on_event_model:ident, $on_event_context:ident) => $on_event: block
+            on_start($on_start_model:ident, $on_start_context:ident) $on_start:block
+            on_event($on_event_event:ident, $on_event_model:ident, $on_event_context:ident) $on_event: block
         }
         $($remainder:tt)+) => {
             swim_agent!{@ {
                 $name, $config,
-                [$($element: $ty)* $lane_name: ValueLane<$key>]
+                [$($element: $ty)* $lane_name: crate::agent::lane::model::value::ValueLane<$key>]
                 [
                     $($out_impl)*
 
@@ -64,13 +62,13 @@ macro_rules! swim_agent {
                         }
                     }
 
-                    impl LaneLifecycle<$config> for $lifecycle {
+                    impl crate::agent::lane::lifecycle::LaneLifecycle<$config> for $lifecycle {
                         fn create(_config: &$config) -> Self {
                             $lifecycle {}
                         }
                     }
 
-                    impl StatefulLaneLifecycleBase for $lifecycle {
+                    impl crate::agent::lane::lifecycle::StatefulLaneLifecycleBase for $lifecycle {
                         type WatchStrategy = $watch_type;
 
                         fn create_strategy(&self) -> Self::WatchStrategy {
@@ -83,13 +81,13 @@ macro_rules! swim_agent {
     // map lane
     (@ { $name: ident, $config: ident, [$($element: ident: $ty: ty)*] [$($out_impl:tt)*] }
         lane(vis: $visibility: ty, path: $path: tt, $lane_name:ident : MapLane<$key: ident, $value: ident>, lifecycle: $lifecycle:ident, watch: $watch_type:ty) => {
-            on_start($on_start_model:ident, $on_start_context:ident) => $on_start:block
-            on_event($on_event_event:ident, $on_event_model:ident, $on_event_context:ident) => $on_event: block
+            on_start($on_start_model:ident, $on_start_context:ident) $on_start:block
+            on_event($on_event_event:ident, $on_event_model:ident, $on_event_context:ident) $on_event: block
         }
         $($remainder:tt)+) => {
             swim_agent!{@ {
                 $name, $config,
-                [$($element: $ty)* $lane_name: MapLane<$key, $value>]
+                [$($element: $ty)* $lane_name: crate::agent::lane::model::map::MapLane<$key, $value>]
                 [
                     $($out_impl)*
 
@@ -112,13 +110,185 @@ macro_rules! swim_agent {
                         }
                     }
 
-                    impl LaneLifecycle<$config> for $lifecycle {
+                    impl crate::agent::lane::lifecycle::LaneLifecycle<$config> for $lifecycle {
                         fn create(_config: &$config) -> Self {
                             $lifecycle {}
                         }
                     }
 
-                    impl StatefulLaneLifecycleBase for $lifecycle {
+                    impl crate::agent::lane::lifecycle::StatefulLaneLifecycleBase for $lifecycle {
+                        type WatchStrategy = $watch_type;
+
+                        fn create_strategy(&self) -> Self::WatchStrategy {
+                            <$watch_type>::default()
+                        }
+                    }
+                ]
+            } $($remainder)*}
+    };
+    // action lane
+    (@ { $name: ident, $config: ident, [$($element: ident: $ty: ty)*] [$($out_impl:tt)*] }
+        lane(vis: $visibility: ty, path: $path: tt, $lane_name:ident : ActionLane<$command_type: ident, $response_type: ident>, lifecycle: $lifecycle:ident, watch: $watch_type:ty) => {
+            on_command($on_command_event:ident, $on_command_model:ident, $on_command_context:ident) -> $response_type_ret: ty $on_command: block
+        }
+        $($remainder:tt)+) => {
+            swim_agent!{@ {
+                $name, $config,
+                [$($element: $ty)* $lane_name: crate::agent::lane::model::action::ActionLane<$command_type, $response_type>]
+                [
+                    $($out_impl)*
+
+                    #[crate::action_lifecycle(agent($name), command_type($command_type), response_type($response_type))]
+                    struct $lifecycle;
+
+                    impl $lifecycle {
+                        async fn on_command<Context>(&self, $on_command_event: $command_type, $on_command_model: &crate::agent::lane::model::action::ActionLane<$command_type, $response_type>, $on_command_context: &Context)
+                        -> $response_type_ret
+                        where
+                            Context: crate::agent::AgentContext<$name> + Sized + Send + Sync,
+                        {
+                            $on_command
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::LaneLifecycle<$config> for $lifecycle {
+                        fn create(_config: &$config) -> Self {
+                            $lifecycle {}
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::StatefulLaneLifecycleBase for $lifecycle {
+                        type WatchStrategy = $watch_type;
+
+                        fn create_strategy(&self) -> Self::WatchStrategy {
+                            <$watch_type>::default()
+                        }
+                    }
+                ]
+            } $($remainder)*}
+    };
+    // command lane
+    (@ { $name: ident, $config: ident, [$($element: ident: $ty: ty)*] [$($out_impl:tt)*] }
+        lane(vis: $visibility: ty, path: $path: tt, $lane_name:ident : CommandLane<$command_type: ident>, lifecycle: $lifecycle:ident, watch: $watch_type:ty) => {
+            on_command($on_command_event:ident, $on_command_model:ident, $on_command_context:ident) $on_command: block
+        }
+        $($remainder:tt)+) => {
+            swim_agent!{@ {
+                $name, $config,
+                [$($element: $ty)* $lane_name: crate::agent::lane::model::action::CommandLane<$command_type>]
+                [
+                    $($out_impl)*
+
+                    #[crate::command_lifecycle(agent($name), command_type($command_type))]
+                    struct $lifecycle;
+
+                    impl $lifecycle {
+                        async fn on_command<Context>(&self, $on_command_event: $command_type, $on_command_model: &crate::agent::lane::model::action::CommandLane<$command_type>, $on_command_context: &Context)
+                        where
+                            Context: crate::agent::AgentContext<$name> + Sized + Send + Sync,
+                        {
+                            $on_command
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::LaneLifecycle<$config> for $lifecycle {
+                        fn create(_config: &$config) -> Self {
+                            $lifecycle {}
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::StatefulLaneLifecycleBase for $lifecycle {
+                        type WatchStrategy = $watch_type;
+
+                        fn create_strategy(&self) -> Self::WatchStrategy {
+                            <$watch_type>::default()
+                        }
+                    }
+                ]
+            } $($remainder)*}
+    };
+    // demand lane
+    (@ { $name: ident, $config: ident, [$($element: ident: $ty: ty)*] [$($out_impl:tt)*] }
+        lane(vis: $visibility: ty, path: $path: tt, $lane_name:ident : DemandLane<$cue_type: ident>, lifecycle: $lifecycle:ident, watch: $watch_type:ty) => {
+            on_cue($on_cue_model:ident, $on_cue_context:ident) -> $cue_type_ret:ty $on_cue: block
+        }
+        $($remainder:tt)+) => {
+            swim_agent!{@ {
+                $name, $config,
+                [$($element: $ty)* $lane_name: crate::agent::lane::model::demand::DemandLane<$cue_type>]
+                [
+                    $($out_impl)*
+
+                    #[crate::demand_lifecycle(agent($name), event_type($cue_type))]
+                    struct $lifecycle;
+
+                    impl $lifecycle {
+                        async fn on_cue<Context>(&self, $on_cue_model: &crate::agent::lane::model::demand::DemandLane<$cue_type>, $on_cue_context: &Context)
+                        -> $cue_type_ret
+                        where
+                            Context: crate::agent::AgentContext<$name> + Sized + Send + Sync,
+                        {
+                            $on_cue
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::LaneLifecycle<$config> for $lifecycle {
+                        fn create(_config: &$config) -> Self {
+                            $lifecycle {}
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::StatefulLaneLifecycleBase for $lifecycle {
+                        type WatchStrategy = $watch_type;
+
+                        fn create_strategy(&self) -> Self::WatchStrategy {
+                            <$watch_type>::default()
+                        }
+                    }
+                ]
+            } $($remainder)*}
+    };
+    // demand map lane
+    (@ { $name: ident, $config: ident, [$($element: ident: $ty: ty)*] [$($out_impl:tt)*] }
+        lane(vis: $visibility: ty, path: $path: tt, $lane_name:ident : DemandMapLane<$key_type: ident, $value_type: ident>, lifecycle: $lifecycle:ident, watch: $watch_type:ty) => {
+            on_cue($on_cue_model:ident, $on_cue_context:ident, $on_cue_key:ident) -> $cue_type_ret:ty $on_cue: block
+            on_sync($on_sync_model:ident, $on_sync_context:ident) -> $sync_type_ret:ty $on_sync: block
+        }
+        $($remainder:tt)+) => {
+            swim_agent!{@ {
+                $name, $config,
+                [$($element: $ty)* $lane_name: crate::agent::lane::model::demand_map::DemandMapLane<$key_type, $value_type>]
+                [
+                    $($out_impl)*
+
+                    #[crate::demand_map_lifecycle(agent($name), key_type($key_type), value_type($value_type))]
+                    struct $lifecycle;
+
+                    impl $lifecycle {
+                        async fn on_cue<Context>(&self, $on_cue_model: &crate::agent::lane::model::demand_map::DemandMapLane<$key_type, $value_type>, $on_cue_context: &Context, $on_cue_key: $key_type)
+                        -> $cue_type_ret
+                        where
+                            Context: crate::agent::AgentContext<$name> + Sized + Send + Sync,
+                        {
+                            $on_cue
+                        }
+
+                        async fn on_sync<Context>(&self, $on_sync_model: &crate::agent::lane::model::demand_map::DemandMapLane<$key_type, $value_type>, $on_sync_context: &Context)
+                        -> $sync_type_ret
+                        where
+                            Context: crate::agent::AgentContext<$name> + Sized + Send + Sync,
+                        {
+                            $on_sync
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::LaneLifecycle<$config> for $lifecycle {
+                        fn create(_config: &$config) -> Self {
+                            $lifecycle {}
+                        }
+                    }
+
+                    impl crate::agent::lane::lifecycle::StatefulLaneLifecycleBase for $lifecycle {
                         type WatchStrategy = $watch_type;
 
                         fn create_strategy(&self) -> Self::WatchStrategy {
@@ -148,20 +318,47 @@ macro_rules! swim_agent {
 
 swim_agent! {
     (SwimAgent, SwimAgentConfig) => {
-        lane(vis: public, path: "/something/:id", value_lane: ValueLane<String>, lifecycle: ValueLifecycle, watch: Queue) => {
-            on_start(_model, _context) => {
+        lane(vis: public, path: "/value_lane/:id", value_lane: ValueLane<String>, lifecycle: ValueLifecycle, watch: Queue) => {
+            on_start(_model, _context) {
                 println!("value lane on start");
             }
-            on_event(event, _model, _context) => {
+            on_event(event, _model, _context) {
                 println!("value lane on event: {}", event);
             }
         }
-        lane(vis: public, path: "/something/:id", map_lane: MapLane<String, i32>, lifecycle: MapLifecycle, watch: Queue) => {
-            on_start(_model, _context) => {
+        lane(vis: public, path: "/map_lane/:id", map_lane: MapLane<String, i32>, lifecycle: MapLifecycle, watch: Queue) => {
+            on_start(_model, _context) {
                 println!("map lane on start");
             }
-            on_event(event, _model, _context) => {
+            on_event(event, _model, _context) {
                 println!("map lane on event: {:?}", event);
+            }
+        }
+        lane(vis: public, path: "/action_lane/:id", action_lane: ActionLane<String, i32>, lifecycle: ActionLifecycle, watch: Queue) => {
+            on_command(command, _model, _context) -> i32 {
+                println!("Command lane received: {}", command);
+                1
+            }
+        }
+        lane(vis: public, path: "/action_lane/:id", command_lane: CommandLane<String>, lifecycle: CommandLifecycle, watch: Queue) => {
+            on_command(command, _model, _context) {
+                println!("Command lane received: {}", command);
+            }
+        }
+        lane(vis: public, path: "/demand_lane/:id", demand_lane: DemandLane<String>, lifecycle: DemandLifecycle, watch: Queue) => {
+            on_cue(_model, _context) -> Option<String> {
+                println!("Demand lane cue");
+                Some(1.to_string())
+            }
+        }
+        lane(vis: public, path: "/demand_map_lane/:id", demand_map_lane: DemandMapLane<String, i32>, lifecycle: DemandMapLifecycle, watch: Queue) => {
+            on_cue(_model, _context, _key) -> Option<i32> {
+                println!("Demand map lane cue");
+                Some(1)
+            }
+            on_sync(_model, _context) -> Vec<String> {
+                println!("Demand map sync");
+                vec!["a".to_string(), "b".to_string(), "c".to_string()]
             }
         }
     }
