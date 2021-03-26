4fbd4df67ec9880be406b2dc52d81c7e56cb8370
Dobromir Marinov <mr.d.marinov@gmail.com>
2021-03-26 13:56:42 +0000

    * Added unit tests.

--- utilities/src/instant/mod.rs
+++ utilities/src/instant/mod.rs
@@ -2,6 +2,12 @@ use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::Duration;
 use tokio::time::Instant;
 
+#[cfg(test)]
+mod tests;
+
+/// An Instant type which can be safely shared between threads.
+///
+/// The offset is stored in milliseconds.
 #[derive(Debug)]
 pub struct AtomicInstant {
     base: Instant,
@@ -16,12 +22,28 @@ impl AtomicInstant {
         }
     }
 
+    /// Loads a value from the atomic Instant.
+    ///
+    /// `load` takes an [`Ordering`] argument which describes the memory ordering
+    /// of this operation. Possible values are [`SeqCst`], [`Acquire`] and [`Relaxed`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if `order` is [`Release`] or [`AcqRel`].
     pub fn load(&self, order: Ordering) -> Instant {
         let offset_millis = self.offset.load(order);
         let offset = Duration::from_millis(offset_millis);
         self.base + offset
     }
 
+    /// Stores a value into the atomic Instant.
+    ///
+    /// `store` takes an [`Ordering`] argument which describes the memory ordering of this operation.
+    ///  Possible values are [`SeqCst`], [`Release`] and [`Relaxed`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if `order` is [`Acquire`] or [`AcqRel`].
     pub fn store(&self, val: Instant, order: Ordering) {
         let offset = val - self.base;
         self.offset.store(offset.as_millis() as u64, order);
