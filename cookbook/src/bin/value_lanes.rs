c6bc27c80ce4e673ddbe3be9f9b49474a00d4ee1
Dobromir Marinov <mr.d.marinov@gmail.com>
2021-04-01 17:27:45 +0100

    * Updated example for easier testing.

--- cookbook/src/bin/value_lanes.rs
+++ cookbook/src/bin/value_lanes.rs
@@ -47,36 +47,15 @@ async fn did_set(value_recv: ValueDownlinkReceiver<String>, initial_value: Strin
 async fn main() {
     let mut client = SwimClientBuilder::build_with_default().await;
     let host_uri = url::Url::parse(&"ws://127.0.0.1:9001".to_string()).unwrap();
-    let node_uri = "unit/foo";
-    let lane_uri = "info";
+    let node_uri = "/rust";
+    let lane_uri = "counter";
 
     let path = AbsolutePath::new(host_uri, node_uri, lane_uri);
-    let (value_downlink, value_recv) = client
-        .value_downlink(path.clone(), String::new())
-        .await
-        .expect("Failed to create value downlink!");
 
-    let initial_value = value_downlink
-        .get()
-        .await
-        .expect("Failed to retrieve initial downlink value!");
-
-    task::spawn(did_set(value_recv, initial_value));
-
-    // Send using either the proxy command lane...
     client
-        .send_command(path, "Hello from command, world!".to_string())
+        .send_command(path, 10)
         .await
         .expect("Failed to send command!");
-    delay_for(Duration::from_secs(2)).await;
 
-    // ...or a downlink set()
-    value_downlink
-        .set("Hello from link, world!".to_string())
-        .await
-        .expect("Failed to send message!");
-    delay_for(Duration::from_secs(2)).await;
-
-    println!("Stopping client in 2 seconds");
     delay_for(Duration::from_secs(2)).await;
 }
