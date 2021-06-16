6830a3b0dea243adce425eb40068a0ab7f95edf8
SirCipher <tklapwijk92@gmail.com>
2021-06-16 14:45:53 +0100

    Adds OpCode structure

--- ratchet/src/lib.rs
+++ ratchet/src/lib.rs
@@ -11,3 +11,61 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use derive_more::Display;
+use std::convert::TryFrom;
+use thiserror::Error;
+
+#[derive(Display)]
+enum OpCode {
+    #[display(fmt = "{}", _0)]
+    DataCode(DataCode),
+    #[display(fmt = "{}", _0)]
+    ControlCode(ControlCode),
+}
+
+#[derive(Display)]
+enum DataCode {
+    #[display(fmt = "Continuation")]
+    Continuation = 0,
+    #[display(fmt = "Text")]
+    Text = 1,
+    #[display(fmt = "Binary")]
+    Binary = 2,
+}
+
+#[derive(Display)]
+enum ControlCode {
+    #[display(fmt = "Close")]
+    Close = 8,
+    #[display(fmt = "Ping")]
+    Ping = 9,
+    #[display(fmt = "Pong")]
+    Pong = 10,
+}
+
+#[derive(Debug, Error)]
+enum OpCodeParseErr {
+    #[error("Reserved OpCode: `{0}`")]
+    Reserved(u8),
+    #[error("Invalid OpCode: `{0}`")]
+    Invalid(u8),
+}
+
+impl TryFrom<u8> for OpCode {
+    type Error = OpCodeParseErr;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(OpCode::DataCode(DataCode::Continuation)),
+            1 => Ok(OpCode::DataCode(DataCode::Text)),
+            2 => Ok(OpCode::DataCode(DataCode::Binary)),
+            r @ 3..=7 => Err(OpCodeParseErr::Reserved(r)),
+            8 => Ok(OpCode::ControlCode(ControlCode::Close)),
+            9 => Ok(OpCode::ControlCode(ControlCode::Ping)),
+            10 => Ok(OpCode::ControlCode(ControlCode::Pong)),
+            r @ 11..=15 => Err(OpCodeParseErr::Reserved(r)),
+            e => Err(OpCodeParseErr::Invalid(e)),
+        }
+    }
+}
