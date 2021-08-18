66bf9582e3743659d8547c1dc7af89a0376ce8e7
Greg Holland <greg.holland@swim.ai>
2021-08-18 14:05:22 +0100

    Fixed test.

--- swim_common/tests/derivetag.rs
+++ swim_common/tests/derivetag.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::str::FromStr;
 use swim_common::form::structural::Tag;
 use swim_common::model::text::Text;
 
