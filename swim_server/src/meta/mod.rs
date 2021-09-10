dec854332f6228d738ecf7cf4a43535693ce9fbd
SirCipher <tklapwijk92@gmail.com>
2021-09-10 13:19:50 +0100

    Adds a graceful staged shutdown to the metrics system

--- swim_server/src/meta/mod.rs
+++ swim_server/src/meta/mod.rs
@@ -313,6 +313,7 @@ where
 
     while let Some(item) = stream.next().await {
         accumulated = accumulated.add(item);
+        println!("Accumulated: {:?}", accumulated);
     }
 
     accumulated
