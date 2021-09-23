edcbd744a5e872a3474660a95facc86edc79ccb9
Greg Holland <greg.holland@swim.ai>
2021-09-23 13:22:07 +0100

    Removed unused code.

--- utilities/src/errors/mod.rs
+++ utilities/src/errors/mod.rs
@@ -12,34 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use futures::task::{Context, Poll};
-use futures::Future;
 use std::error::Error;
-use std::pin::Pin;
-
-pub struct FlattenErrors<F> {
-    inner: F,
-}
-
-impl<F: Unpin> Unpin for FlattenErrors<F> {}
-
-impl<F> FlattenErrors<F> {
-    pub fn new(inner: F) -> Self {
-        FlattenErrors { inner }
-    }
-}
-
-impl<F, T, E> Future for FlattenErrors<F>
-where
-    F: Future<Output = Result<Result<T, E>, E>> + Unpin,
-{
-    type Output = Result<T, E>;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let f = &mut self.get_mut().inner;
-        Pin::new(f).poll(cx).map(|r| r.and_then(|r2| r2))
-    }
-}
 
 /// A trait from errors for which it is potentially possible to recover.
 pub trait Recoverable: Error {
@@ -47,17 +20,5 @@ pub trait Recoverable: Error {
 
     fn is_transient(&self) -> bool {
         !self.is_fatal()
-    }
-}
-
-pub trait SwimResultExt<O, E> {
-    /// Maps the error variant to `()`.
-    #[allow(clippy::result_unit_err)]
-    fn discard_err(self) -> Result<O, ()>;
-}
-
-impl<O, E> SwimResultExt<O, E> for Result<O, E> {
-    fn discard_err(self) -> Result<O, ()> {
-        self.map_err(|_| ())
     }
 }
