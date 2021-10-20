b92ee06143c20f84b70e1efe25613fe0fc460d9a
SirCipher <tklapwijk92@gmail.com>
2021-10-20 15:43:31 +0100

    Removes duplicate nostore implementation

--- swim_server/src/store/mod.rs
+++ swim_server/src/store/mod.rs
@@ -12,11 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod keystore;
 #[cfg(test)]
 pub mod mock;
-
-pub mod keystore;
-mod nostore;
 
 #[cfg(feature = "persistence")]
 pub mod rocks;
