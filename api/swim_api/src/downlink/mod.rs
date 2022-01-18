e4dde94e31a0975cdc362048bbb27d007e56e032
Greg Holland <greg.holland@swim.ai>
2022-01-18 11:48:54 +0000

    Added downlink trait.

---
+++ api/swim_api/src/downlink/mod.rs
@@ -0,0 +1,44 @@
+// Copyright 2015-2021 Swim Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::time::Duration;
+
+use futures::future::BoxFuture;
+use swim_model::path::AbsolutePath;
+use swim_utilities::io::byte_channel::{ByteReader, ByteWriter};
+
+use crate::error::DownlinkTaskError;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum DownlinkKind {
+    Value,
+    Event,
+    Map,
+}
+
+pub struct DownlinkConfig {
+    _flush_timeout: Duration,
+}
+
+pub trait Downlink {
+    fn kind(&self) -> DownlinkKind;
+
+    fn run(
+        &self,
+        path: AbsolutePath,
+        config: DownlinkConfig,
+        input: ByteReader,
+        output: ByteWriter,
+    ) -> BoxFuture<'static, Result<(), DownlinkTaskError>>;
+}
---
+++ api/swim_api/src/error/mod.rs
@@ -0,0 +1,35 @@
+// Copyright 2015-2021 Swim Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use swim_form::structural::read::ReadError;
+use swim_model::Text;
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone, PartialEq, Eq)]
+pub enum InvalidFrame {
+    #[error("An incoming frame was incomplete.")]
+    Incomplete,
+    #[error("Invalid frame header: {problem}")]
+    InvalidHeader { problem: Text },
+    #[error("Invalid frame body: {problem}")]
+    InvalidMessageBody { body: Vec<u8>, problem: Text },
+}
+
+#[derive(Error, Debug, Clone, PartialEq, Eq)]
+pub enum DownlinkTaskError {
+    #[error("{0}")]
+    BadFrame(#[from] InvalidFrame),
+    #[error("Failed to deserialize frame body: {0}")]
+    DeserializationFailed(#[from] ReadError),
+}
