6ec8bf5ebbaed5bd3a9559d847bfb0174717f316
Dobromir Marinov <mr.d.marinov@gmail.com>
2022-01-21 15:28:50 +0000

    * Refactored hasher tests.

--- api/formats/swim_recon/src/hasher/tests.rs
+++ api/formats/swim_recon/src/hasher/tests.rs
@@ -7,331 +7,190 @@ fn value_from_string(rep: &str) -> Result<Value, ParseError> {
     crate::parser::parse_recognize(span, false)
 }
 
-#[test]
-fn recon_hash_not_eq() {
-    let first = "@foo({1})";
-    let second = "@name({a: 1, b: 2})";
+fn cmp_eq(first: &str, second: &str) {
+    assert_eq!(
+        calculate_hash(first).unwrap(),
+        calculate_hash(second).unwrap()
+    );
 
+    let result_1 = value_from_string(first).unwrap();
+    let result_2 = value_from_string(second).unwrap();
+    assert_eq!(result_1, result_2);
+}
+
+fn cmp_ne(first: &str, second: &str) {
     assert_ne!(
         calculate_hash(first).unwrap(),
         calculate_hash(second).unwrap()
     );
 
-    let first = "@foo({})";
-    let second = "@foo()";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@attr({{}})";
-    let second = "@attr({})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@foo({1})";
-    let second = "@foo(1)";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@name(@foo@bar)";
-    let second = "@name({@foo@bar})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@foo(@bar {})";
-    let second = "@foo(@bar, {})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    //Todo dm
-
-    let first = "@name(a: 1, b: 2, c: 3)";
-    let second = "@name(a:1, b: 4, c: 3)";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@foo({{1,2}})";
-    let second = "@foo({1, 2})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@name(a: 1, b: 2)";
-    let second = "@name(   {a: 3, b: 2}    )";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@foo(1)";
-    let second = "@foo({1})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@foo()";
-    let second = "@foo({})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@foo(@bar@baz) ";
-    let second = "@foo({@bar@baz})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@name({1,2},{3,4})";
-    let second = "@name({1,2,3,4})";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    let first = "@foo(1, @bar(2), 3, 4)";
-    let second = "@foo(1, @bar({2}), 3, 4)";
-
-    assert_ne!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    let result_1 = value_from_string(first).unwrap();
+    let result_2 = value_from_string(second).unwrap();
+    assert_ne!(result_1, result_2);
 }
 
 #[test]
 fn recon_hash_eq() {
     let first = "@name(a: 1, b: 2)";
     let second = "@name({a: 1, b: 2})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@foo({a: 1})";
     let second = "@foo(a: 1)";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@foo(a:)";
     let second = "@foo({a: })";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name({a: 1, b: 2}, 3)";
     let second = "@name({{a: 1, b: 2}, 3})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(3, {a: 1, b: 2})";
     let second = "@name({3, {a: 1, b: 2}})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@attr({{1,2},{3,4}})";
     let second = "@attr({1,2},{3,4})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@attr({1,2,3,4})";
     let second = "@attr(1,2,3,4)";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@foo({one: 1, two: @bar(1,2,3), three: 3, four: {@baz({1,2})}})";
     let second = "@foo(one: 1, two: @bar({1,2,3}), three: 3, four: {@baz(1,2)})";
+    cmp_eq(first, second);
 
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
-
-    //Todo dm
     let first = "@name(a: 1, b: 2)";
     let second = "@name(a: 1, b: 2)";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "\"test\"";
     let second = "test";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "{a:2}";
     let second = "{ a: 2 }";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@tag(){}:1";
     let second = "@tag{}:1";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(a: 1, b: 2)";
     let second = "@name({a: 1, b: 2})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@first(1)@second(2)";
     let second = "@first(1)@second(2) {}";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "{ @inner(0), after }";
     let second = "{ @inner(0) {}, after }";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@outer(@inner)";
     let second = "@outer(@inner {})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@foo({one: 1, two: @bar(1,2,3), three: 3, four: {@baz({1,2})}})";
     let second = "@foo(one: 1, two: @bar({1,2,3}), three: 3, four: {@baz(1,2)})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@foo(1,2)";
     let second = "@foo({1,2})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@foo({one: 1, two: @bar(1,2,3)})";
     let second = "@foo(one: 1, two: @bar({1,2,3}))";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(@foo,@bar)";
     let second = "@name({@foo, @bar})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(1, @foo@bar)";
     let second = "@name({1, @foo@bar})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(@foo@bar({@bar, 1, @baz()}), @bar@foo()@baz)";
     let second = "@name({@foo@bar(@bar, 1, @baz()), @bar@foo()@baz})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@foo(1, @bar(2,3), 4, 5)";
     let second = "@foo(1, @bar({2,3}), 4, 5)";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(a: @foo)";
     let second = "@name({a: @foo})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(b: @foo@bar@baz, @foo(1,2), @bar@baz))";
     let second = "@name({b: @foo@bar@baz, @foo({1,2}), @bar@baz})";
-
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+    cmp_eq(first, second);
 
     let first = "@name(b: )";
     let second = "@name({b: })";
+    cmp_eq(first, second);
+}
 
-    assert_eq!(
-        calculate_hash(first).unwrap(),
-        calculate_hash(second).unwrap()
-    );
+#[test]
+fn recon_hash_not_eq() {
+    let first = "@foo({1})";
+    let second = "@name({a: 1, b: 2})";
+    cmp_ne(first, second);
+
+    let first = "@foo({})";
+    let second = "@foo()";
+    cmp_ne(first, second);
+
+    let first = "@attr({{}})";
+    let second = "@attr({})";
+    cmp_ne(first, second);
+
+    let first = "@foo({1})";
+    let second = "@foo(1)";
+    cmp_ne(first, second);
+
+    let first = "@name(@foo@bar)";
+    let second = "@name({@foo@bar})";
+    cmp_ne(first, second);
+
+    let first = "@foo(@bar {})";
+    let second = "@foo(@bar, {})";
+    cmp_ne(first, second);
+
+    let first = "@name(a: 1, b: 2, c: 3)";
+    let second = "@name(a:1, b: 4, c: 3)";
+    cmp_ne(first, second);
+
+    let first = "@foo({{1,2}})";
+    let second = "@foo({1, 2})";
+    cmp_ne(first, second);
+
+    let first = "@name(a: 1, b: 2)";
+    let second = "@name(   {a: 3, b: 2}    )";
+    cmp_ne(first, second);
+
+    let first = "@foo(1)";
+    let second = "@foo({1})";
+    cmp_ne(first, second);
+
+    let first = "@foo()";
+    let second = "@foo({})";
+    cmp_ne(first, second);
+
+    let first = "@foo(@bar@baz) ";
+    let second = "@foo({@bar@baz})";
+    cmp_ne(first, second);
+
+    let first = "@name({1,2},{3,4})";
+    let second = "@name({1,2,3,4})";
+    cmp_ne(first, second);
+
+    let first = "@foo(1, @bar(2), 3, 4)";
+    let second = "@foo(1, @bar({2}), 3, 4)";
+    cmp_ne(first, second);
 }
