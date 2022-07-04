ef2410fe9317355a45852340eb77b0e80520162c
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2022-07-04 15:37:25 +0100

    Added map lanes.

--- swim_agent/src/lanes/map/mod.rs
+++ swim_agent/src/lanes/map/mod.rs
@@ -12,4 +12,467 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use bytes::BytesMut;
+use std::{borrow::Borrow, cell::RefCell, collections::HashMap, hash::Hash};
+use swim_api::protocol::{
+    agent::{LaneResponseKind, MapLaneResponse, MapLaneResponseEncoder},
+    map::MapOperation,
+};
+use swim_form::structural::write::StructuralWritable;
+use tokio_util::codec::Encoder;
+use uuid::Uuid;
+
+mod event;
 pub mod lifecycle;
+mod queues;
+
+use crate::{
+    event_handler::{EventHandler, StepResult},
+    meta::AgentMetadata,
+    model::WriteResult,
+};
+
+use self::queues::{Action, ToWrite, WriteQueues};
+
+pub use event::MapLaneEvent;
+
+pub struct MapLane<K, V> {
+    id: u64,
+    inner: RefCell<Inner<K, V>>,
+}
+
+impl<K, V> MapLane<K, V> {
+    pub fn new(id: u64, init: HashMap<K, V>) -> Self {
+        MapLane {
+            id,
+            inner: RefCell::new(Inner::new(init)),
+        }
+    }
+}
+
+impl<K, V> MapLane<K, V>
+where
+    K: Clone + Eq + Hash,
+{
+    pub fn update(&self, key: K, value: V) {
+        self.inner.borrow_mut().update(key, value)
+    }
+
+    pub fn remove(&self, key: &K) {
+        self.inner.borrow_mut().remove(key)
+    }
+
+    pub fn clear(&self) {
+        self.inner.borrow_mut().clear()
+    }
+
+    pub fn get<Q, F, R>(&self, key: &Q, f: F) -> R
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+        F: FnOnce(Option<&V>) -> R,
+    {
+        self.inner.borrow().get(key, f)
+    }
+
+    pub fn get_map<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&HashMap<K, V>) -> R,
+    {
+        self.inner.borrow().get_map(f)
+    }
+
+    pub(crate) fn read_with_prev<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(Option<MapLaneEvent<K, V>>, &HashMap<K, V>) -> R,
+    {
+        self.inner.borrow_mut().read_with_prev(f)
+    }
+
+    pub fn sync(&self, id: Uuid) {
+        self.inner.borrow_mut().sync(id);
+    }
+}
+
+impl<K, V> MapLane<K, V>
+where
+    K: Clone + Eq + Hash + StructuralWritable,
+    V: StructuralWritable,
+{
+    pub fn write_to_buffer(&self, buffer: &mut BytesMut) -> WriteResult {
+        self.inner.borrow_mut().write_to_buffer(buffer)
+    }
+}
+
+struct Inner<K, V> {
+    content: HashMap<K, V>,
+    previous: Option<MapLaneEvent<K, V>>,
+    write_queues: WriteQueues<K>,
+}
+
+impl<K, V> Inner<K, V> {
+    fn new(init: HashMap<K, V>) -> Self {
+        Inner {
+            content: init,
+            previous: None,
+            write_queues: Default::default(),
+        }
+    }
+}
+
+impl<K, V> Inner<K, V>
+where
+    K: Clone + Eq + Hash,
+{
+    pub fn update(&mut self, key: K, value: V) {
+        let Inner {
+            content,
+            previous,
+            write_queues,
+        } = self;
+        let prev = content.insert(key.clone(), value);
+        *previous = Some(MapLaneEvent::Update(key.clone(), prev));
+        write_queues.push_update(key);
+    }
+
+    pub fn remove(&mut self, key: &K) {
+        let Inner {
+            content,
+            previous,
+            write_queues,
+        } = self;
+        let prev = content.remove(key);
+        if let Some(prev) = prev {
+            *previous = Some(MapLaneEvent::Remove(key.clone(), prev));
+            write_queues.push_remove(key.clone());
+        }
+    }
+
+    pub fn clear(&mut self) {
+        let Inner {
+            content,
+            previous,
+            write_queues,
+        } = self;
+        *previous = Some(MapLaneEvent::Clear(std::mem::take(content)));
+        write_queues.push_clear();
+    }
+
+    pub fn get<Q, F, R>(&self, key: &Q, f: F) -> R
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+        F: FnOnce(Option<&V>) -> R,
+    {
+        let Inner { content, .. } = self;
+        f(content.get(key))
+    }
+
+    pub fn get_map<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&HashMap<K, V>) -> R,
+    {
+        let Inner { content, .. } = self;
+        f(content)
+    }
+
+    pub(crate) fn read_with_prev<F, R>(&mut self, f: F) -> R
+    where
+        F: FnOnce(Option<MapLaneEvent<K, V>>, &HashMap<K, V>) -> R,
+    {
+        let Inner {
+            content, previous, ..
+        } = self;
+        f(previous.take(), content)
+    }
+
+    pub fn sync(&mut self, id: Uuid) {
+        let Inner {
+            content,
+            write_queues,
+            ..
+        } = self;
+        write_queues.sync(id, content.keys().cloned());
+    }
+}
+
+const INFALLIBLE_SER: &str = "Serializing map operations to recon should be infallible.";
+
+impl<K, V> Inner<K, V>
+where
+    K: Clone + Eq + Hash + StructuralWritable,
+    V: StructuralWritable,
+{
+    pub fn write_to_buffer(&mut self, buffer: &mut BytesMut) -> WriteResult {
+        let Inner {
+            content,
+            write_queues,
+            ..
+        } = self;
+        loop {
+            let maybe_response = match write_queues.pop() {
+                Some(ToWrite::Event(event)) => {
+                    to_operation(content, event).map(|operation| MapLaneResponse::Event {
+                        kind: LaneResponseKind::StandardEvent,
+                        operation,
+                    })
+                }
+                Some(ToWrite::SyncEvent(id, key)) => to_operation(content, Action::Update(key))
+                    .map(|operation| MapLaneResponse::Event {
+                        kind: LaneResponseKind::SyncEvent(id),
+                        operation,
+                    }),
+                Some(ToWrite::Synced(id)) => Some(MapLaneResponse::SyncComplete(id)),
+                _ => break WriteResult::NoData,
+            };
+            if let Some(response) = maybe_response {
+                let mut encoder = MapLaneResponseEncoder::default();
+                encoder.encode(response, buffer).expect(INFALLIBLE_SER);
+                break if write_queues.is_empty() {
+                    WriteResult::Done
+                } else {
+                    WriteResult::DataStillAvailable
+                };
+            } else {
+                continue;
+            }
+        }
+    }
+}
+
+fn to_operation<K: Eq + Hash, V>(
+    content: &HashMap<K, V>,
+    action: Action<K>,
+) -> Option<MapOperation<K, &V>> {
+    match action {
+        Action::Update(k) => content
+            .get(&k)
+            .map(|v| MapOperation::Update { key: k, value: v }),
+        Action::Remove(k) => Some(MapOperation::Remove { key: k }),
+        Action::Clear => Some(MapOperation::Clear),
+    }
+}
+
+pub struct MapLaneUpdate<C, K, V> {
+    projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>,
+    key_value: Option<(K, V)>,
+}
+
+impl<C, K, V> MapLaneUpdate<C, K, V> {
+    pub fn new(projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>, key: K, value: V) -> Self {
+        MapLaneUpdate {
+            projection,
+            key_value: Some((key, value)),
+        }
+    }
+}
+
+impl<C, K, V> EventHandler<C> for MapLaneUpdate<C, K, V>
+where
+    K: Clone + Eq + Hash,
+{
+    type Completion = ();
+
+    fn step(&mut self, _meta: AgentMetadata, context: &C) -> StepResult<Self::Completion> {
+        let MapLaneUpdate {
+            projection,
+            key_value,
+        } = self;
+        if let Some((key, value)) = key_value.take() {
+            let lane = projection(context);
+            lane.update(key, value);
+            StepResult::Complete {
+                modified_lane: Some(lane.id),
+                result: (),
+            }
+        } else {
+            StepResult::after_done()
+        }
+    }
+}
+
+pub struct MapLaneRemove<C, K, Q, V> {
+    projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>,
+    key: Option<Q>,
+}
+
+impl<C, K, Q, V> MapLaneRemove<C, K, Q, V> {
+    pub fn new(projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>, key: Q) -> Self {
+        MapLaneRemove {
+            projection,
+            key: Some(key),
+        }
+    }
+}
+
+impl<C, K, Q, V> EventHandler<C> for MapLaneRemove<C, K, Q, V>
+where
+    K: Clone + Eq + Hash,
+    Q: AsRef<K>,
+{
+    type Completion = ();
+
+    fn step(&mut self, _meta: AgentMetadata, context: &C) -> StepResult<Self::Completion> {
+        let MapLaneRemove { projection, key } = self;
+        if let Some(key) = key.take() {
+            let lane = projection(context);
+            lane.remove(key.as_ref());
+            StepResult::Complete {
+                modified_lane: Some(lane.id),
+                result: (),
+            }
+        } else {
+            StepResult::after_done()
+        }
+    }
+}
+
+pub struct MapLaneClear<C, K, V> {
+    projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>,
+    done: bool,
+}
+
+impl<C, K, V> MapLaneClear<C, K, V> {
+    pub fn new(projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>) -> Self {
+        MapLaneClear {
+            projection,
+            done: false,
+        }
+    }
+}
+
+impl<C, K, V> EventHandler<C> for MapLaneClear<C, K, V>
+where
+    K: Clone + Eq + Hash,
+{
+    type Completion = ();
+
+    fn step(&mut self, _meta: AgentMetadata, context: &C) -> StepResult<Self::Completion> {
+        let MapLaneClear { projection, done } = self;
+        if !*done {
+            *done = true;
+            let lane = projection(context);
+            lane.clear();
+            StepResult::Complete {
+                modified_lane: Some(lane.id),
+                result: (),
+            }
+        } else {
+            StepResult::after_done()
+        }
+    }
+}
+
+pub struct MapLaneGet<C, K, Q, V> {
+    projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>,
+    key: Q,
+    done: bool,
+}
+
+impl<C, K, Q, V> MapLaneGet<C, K, Q, V> {
+    pub fn new(projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>, key: Q) -> Self {
+        MapLaneGet {
+            projection,
+            key,
+            done: false,
+        }
+    }
+}
+
+impl<C, K, Q, V> EventHandler<C> for MapLaneGet<C, K, Q, V>
+where
+    K: Clone + Eq + Hash,
+    V: Clone,
+    Q: AsRef<K>,
+{
+    type Completion = Option<V>;
+
+    fn step(&mut self, _meta: AgentMetadata, context: &C) -> StepResult<Self::Completion> {
+        let MapLaneGet {
+            projection,
+            key,
+            done,
+        } = self;
+        if !*done {
+            *done = true;
+            let lane = projection(context);
+            StepResult::Complete {
+                modified_lane: Some(lane.id),
+                result: lane.get(key.as_ref(), |v| v.cloned()),
+            }
+        } else {
+            StepResult::after_done()
+        }
+    }
+}
+
+pub struct MapLaneGetMap<C, K, V> {
+    projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>,
+    done: bool,
+}
+
+impl<C, K, V> MapLaneGetMap<C, K, V> {
+    pub fn new(projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>) -> Self {
+        MapLaneGetMap {
+            projection,
+            done: false,
+        }
+    }
+}
+
+impl<C, K, V> EventHandler<C> for MapLaneGetMap<C, K, V>
+where
+    K: Clone + Eq + Hash,
+    V: Clone,
+{
+    type Completion = HashMap<K, V>;
+
+    fn step(&mut self, _meta: AgentMetadata, context: &C) -> StepResult<Self::Completion> {
+        let MapLaneGetMap { projection, done } = self;
+        if !*done {
+            *done = true;
+            let lane = projection(context);
+            StepResult::Complete {
+                modified_lane: Some(lane.id),
+                result: lane.get_map(Clone::clone),
+            }
+        } else {
+            StepResult::after_done()
+        }
+    }
+}
+
+pub struct MapLaneSync<C, K, V> {
+    projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>,
+    id: Option<Uuid>,
+}
+
+impl<C, K, V> MapLaneSync<C, K, V> {
+    pub fn new(projection: for<'a> fn(&'a C) -> &'a MapLane<K, V>, id: Uuid) -> Self {
+        MapLaneSync {
+            projection,
+            id: Some(id),
+        }
+    }
+}
+
+impl<C, K, V> EventHandler<C> for MapLaneSync<C, K, V>
+where
+    K: Clone + Eq + Hash,
+{
+    type Completion = ();
+
+    fn step(&mut self, _meta: AgentMetadata, context: &C) -> StepResult<Self::Completion> {
+        let MapLaneSync { projection, id } = self;
+        if let Some(id) = id.take() {
+            let lane = projection(context);
+            lane.sync(id);
+            StepResult::Complete {
+                modified_lane: Some(lane.id),
+                result: (),
+            }
+        } else {
+            StepResult::after_done()
+        }
+    }
+}
--- swim_agent/src/lifecycle/lane_event/mod.rs
+++ swim_agent/src/lifecycle/lane_event/mod.rs
@@ -12,21 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::cmp::Ordering;
 use std::fmt::Debug;
+use std::hash::Hash;
+use std::{cmp::Ordering, collections::HashMap};
 
+use frunk::{Coprod, Coproduct};
 use futures::future::Either;
 use swim_api::handlers::NoHandler;
 
+use crate::lanes::map::lifecycle::MapLaneLifecycleShared;
 use crate::{
     event_handler::{EventHandler, FollowedBy, UnitHandler},
-    lanes::value::{
-        lifecycle::{
-            on_event::{OnEvent, OnEventShared},
-            on_set::{OnSet, OnSetShared},
-            ValueLaneLifecycle, ValueLaneLifecycleShared,
+    lanes::{
+        map::{
+            lifecycle::{
+                on_clear::{OnClear, OnClearShared},
+                on_remove::{OnRemove, OnRemoveShared},
+                on_update::{OnUpdate, OnUpdateShared},
+                MapLaneLifecycle,
+            },
+            MapLane, MapLaneEvent,
         },
-        ValueLane,
+        value::{
+            lifecycle::{
+                on_event::{OnEvent, OnEventShared},
+                on_set::{OnSet, OnSetShared},
+                ValueLaneLifecycle, ValueLaneLifecycleShared,
+            },
+            ValueLane,
+        },
     },
 };
 
@@ -220,7 +234,7 @@ impl<'a, Context, T, LC> LaneEvent<'a, Context> for ValueLeaf<Context, T, LC>
 where
     LC: ValueLaneLifecycle<T, Context>,
 {
-    type LaneEventHandler = LifecycleHandler<'a, Context, T, LC>;
+    type LaneEventHandler = ValueLifecycleHandler<'a, Context, T, LC>;
 
     fn lane_event(&'a self, context: &Context, lane_name: &str) -> Option<Self::LaneEventHandler> {
         let ValueLeaf {
@@ -246,7 +260,7 @@ impl<'a, Context, Shared, T, LC> LaneEventShared<'a, Context, Shared> for ValueL
 where
     LC: ValueLaneLifecycleShared<T, Context, Shared>,
 {
-    type LaneEventHandler = LifecycleHandlerShared<'a, Context, Shared, T, LC>;
+    type LaneEventHandler = ValueLifecycleHandlerShared<'a, Context, Shared, T, LC>;
 
     fn lane_event(
         &'a self,
@@ -274,25 +288,28 @@ where
     }
 }
 
-pub type LifecycleHandler<'a, Context, T, LC> = FollowedBy<
+pub type ValueLifecycleHandler<'a, Context, T, LC> = FollowedBy<
     <LC as OnEvent<'a, T, Context>>::OnEventHandler,
     <LC as OnSet<'a, T, Context>>::OnSetHandler,
 >;
 
-pub type LifecycleHandlerShared<'a, Context, Shared, T, LC> = FollowedBy<
+pub type ValueLifecycleHandlerShared<'a, Context, Shared, T, LC> = FollowedBy<
     <LC as OnEventShared<'a, T, Context, Shared>>::OnEventHandler,
     <LC as OnSetShared<'a, T, Context, Shared>>::OnSetHandler,
 >;
 
 type ValueBranchHandler<'a, Context, T, LC, L, R> = Either<
     <L as LaneEvent<'a, Context>>::LaneEventHandler,
-    Either<LifecycleHandler<'a, Context, T, LC>, <R as LaneEvent<'a, Context>>::LaneEventHandler>,
+    Either<
+        ValueLifecycleHandler<'a, Context, T, LC>,
+        <R as LaneEvent<'a, Context>>::LaneEventHandler,
+    >,
 >;
 
 type ValueBranchHandlerShared<'a, Context, Shared, T, LC, L, R> = Either<
     <L as LaneEventShared<'a, Context, Shared>>::LaneEventHandler,
     Either<
-        LifecycleHandlerShared<'a, Context, Shared, T, LC>,
+        ValueLifecycleHandlerShared<'a, Context, Shared, T, LC>,
         <R as LaneEventShared<'a, Context, Shared>>::LaneEventHandler,
     >,
 >;
@@ -366,6 +383,336 @@ where
                     event_handler.followed_by(set_handler)
                 });
                 Some(Either::Right(Either::Left(handler)))
+            }
+            Ordering::Greater => right
+                .lane_event(shared, handler_context, context, lane_name)
+                .map(|r| Either::Right(Either::Right(r))),
+        }
+    }
+}
+
+pub struct MapLeaf<Context, K, V, LC> {
+    label: &'static str,
+    projection: fn(&Context) -> &MapLane<K, V>,
+    lifecycle: LC,
+}
+
+impl<Context, K, V, LC> HTree for MapLeaf<Context, K, V, LC> {
+    fn label(&self) -> Option<&'static str> {
+        Some(self.label)
+    }
+}
+
+impl<Context, K, V, LC> MapLeaf<Context, K, V, LC> {
+    pub fn new(
+        label: &'static str,
+        projection: fn(&Context) -> &MapLane<K, V>,
+        lifecycle: LC,
+    ) -> Self {
+        MapLeaf {
+            label,
+            projection,
+            lifecycle,
+        }
+    }
+}
+
+pub type MapLifecycleHandler<'a, Context, K, V, LC> = Coprod!(
+    <LC as OnUpdate<'a, K, V, Context>>::OnUpdateHandler,
+    <LC as OnRemove<'a, K, V, Context>>::OnRemoveHandler,
+    <LC as OnClear<'a, K, V, Context>>::OnClearHandler,
+);
+
+pub type MapLifecycleHandlerShared<'a, Context, Shared, K, V, LC> = Coprod!(
+    <LC as OnUpdateShared<'a, K, V, Context, Shared>>::OnUpdateHandler,
+    <LC as OnRemoveShared<'a, K, V, Context, Shared>>::OnRemoveHandler,
+    <LC as OnClearShared<'a, K, V, Context, Shared>>::OnClearHandler,
+);
+
+type MapBranchHandler<'a, Context, K, V, LC, L, R> = Either<
+    <L as LaneEvent<'a, Context>>::LaneEventHandler,
+    Either<
+        MapLifecycleHandler<'a, Context, K, V, LC>,
+        <R as LaneEvent<'a, Context>>::LaneEventHandler,
+    >,
+>;
+
+type MapBranchHandlerShared<'a, Context, Shared, K, V, LC, L, R> = Either<
+    <L as LaneEventShared<'a, Context, Shared>>::LaneEventHandler,
+    Either<
+        MapLifecycleHandlerShared<'a, Context, Shared, K, V, LC>,
+        <R as LaneEventShared<'a, Context, Shared>>::LaneEventHandler,
+    >,
+>;
+
+impl<Context, K, V, LC> Debug for MapLeaf<Context, K, V, LC>
+where
+    LC: Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ValueLeaf")
+            .field("label", &self.label)
+            .field("projection", &"...")
+            .field("lifecycle", &self.lifecycle)
+            .finish()
+    }
+}
+
+fn map_handler<'a, Context, K, V, LC>(
+    event: MapLaneEvent<K, V>,
+    lifecycle: &'a LC,
+    map: &HashMap<K, V>,
+) -> MapLifecycleHandler<'a, Context, K, V, LC>
+where
+    LC: MapLaneLifecycle<K, V, Context>,
+{
+    match event {
+        MapLaneEvent::Update(k, v) => Coproduct::Inl(lifecycle.on_update(map, k, v)),
+        MapLaneEvent::Remove(k, v) => {
+            Coproduct::Inr(Coproduct::Inl(lifecycle.on_remove(map, k, v)))
+        }
+        MapLaneEvent::Clear(before) => {
+            Coproduct::Inr(Coproduct::Inr(Coproduct::Inl(lifecycle.on_clear(before))))
+        }
+    }
+}
+
+fn map_handler_shared<'a, Context, Shared, K, V, LC>(
+    shared: &'a Shared,
+    handler_context: HandlerContext<Context>,
+    event: MapLaneEvent<K, V>,
+    lifecycle: &'a LC,
+    map: &HashMap<K, V>,
+) -> MapLifecycleHandlerShared<'a, Context, Shared, K, V, LC>
+where
+    LC: MapLaneLifecycleShared<K, V, Context, Shared>,
+{
+    match event {
+        MapLaneEvent::Update(k, v) => {
+            Coproduct::Inl(lifecycle.on_update(shared, handler_context, map, k, v))
+        }
+        MapLaneEvent::Remove(k, v) => Coproduct::Inr(Coproduct::Inl(lifecycle.on_remove(
+            shared,
+            handler_context,
+            map,
+            k,
+            v,
+        ))),
+        MapLaneEvent::Clear(before) => Coproduct::Inr(Coproduct::Inr(Coproduct::Inl(
+            lifecycle.on_clear(shared, handler_context, before),
+        ))),
+    }
+}
+
+impl<'a, Context, K, V, LC> LaneEvent<'a, Context> for MapLeaf<Context, K, V, LC>
+where
+    K: Clone + Eq + Hash,
+    LC: MapLaneLifecycle<K, V, Context>,
+{
+    type LaneEventHandler = MapLifecycleHandler<'a, Context, K, V, LC>;
+
+    fn lane_event(&'a self, context: &Context, lane_name: &str) -> Option<Self::LaneEventHandler> {
+        let MapLeaf {
+            label,
+            projection,
+            lifecycle,
+        } = self;
+        if lane_name == *label {
+            let lane = projection(context);
+            lane.read_with_prev(|prev, map| {
+                if let Some(ev) = prev {
+                    Some(map_handler(ev, lifecycle, map))
+                } else {
+                    None
+                }
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, Context, Shared, K, V, LC> LaneEventShared<'a, Context, Shared>
+    for MapLeaf<Context, K, V, LC>
+where
+    K: Clone + Eq + Hash,
+    LC: MapLaneLifecycleShared<K, V, Context, Shared>,
+{
+    type LaneEventHandler = MapLifecycleHandlerShared<'a, Context, Shared, K, V, LC>;
+
+    fn lane_event(
+        &'a self,
+        shared: &'a Shared,
+        handler_context: HandlerContext<Context>,
+        context: &Context,
+        lane_name: &str,
+    ) -> Option<Self::LaneEventHandler> {
+        let MapLeaf {
+            label,
+            projection,
+            lifecycle,
+        } = self;
+        if lane_name == *label {
+            let lane = projection(context);
+            lane.read_with_prev(|prev, map| {
+                if let Some(ev) = prev {
+                    Some(map_handler_shared(
+                        shared,
+                        handler_context,
+                        ev,
+                        lifecycle,
+                        map,
+                    ))
+                } else {
+                    None
+                }
+            })
+        } else {
+            None
+        }
+    }
+}
+
+pub struct MapBranch<Context, K, V, LC, L, R> {
+    label: &'static str,
+    projection: fn(&Context) -> &MapLane<K, V>,
+    lifecycle: LC,
+    left: L,
+    right: R,
+}
+
+impl<Context, K, V, LC, L: HTree, R: HTree> HTree for MapBranch<Context, K, V, LC, L, R> {
+    fn label(&self) -> Option<&'static str> {
+        Some(self.label)
+    }
+}
+
+impl<Context, K, V, LC, L, R> Debug for MapBranch<Context, K, V, LC, L, R>
+where
+    LC: Debug,
+    L: Debug,
+    R: Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ValueBranch")
+            .field("label", &self.label)
+            .field("projection", &"...")
+            .field("lifecycle", &self.lifecycle)
+            .field("left", &self.left)
+            .field("right", &self.right)
+            .finish()
+    }
+}
+
+impl<Context, K, V, LC, L, R> MapBranch<Context, K, V, LC, L, R>
+where
+    L: HTree,
+    R: HTree,
+{
+    pub fn new(
+        label: &'static str,
+        projection: fn(&Context) -> &MapLane<K, V>,
+        lifecycle: LC,
+        left: L,
+        right: R,
+    ) -> Self {
+        if let Some(left_label) = left.label() {
+            assert!(left_label < label);
+        }
+        if let Some(right_label) = right.label() {
+            assert!(label < right_label);
+        }
+        MapBranch {
+            label,
+            projection,
+            lifecycle,
+            left,
+            right,
+        }
+    }
+}
+
+impl<'a, Context, K, V, LC, L, R> LaneEvent<'a, Context> for MapBranch<Context, K, V, LC, L, R>
+where
+    K: Clone + Eq + Hash,
+    LC: MapLaneLifecycle<K, V, Context>,
+    L: HTree + LaneEvent<'a, Context>,
+    R: HTree + LaneEvent<'a, Context>,
+{
+    type LaneEventHandler = MapBranchHandler<'a, Context, K, V, LC, L, R>;
+
+    fn lane_event(&'a self, context: &Context, lane_name: &str) -> Option<Self::LaneEventHandler> {
+        let MapBranch {
+            label,
+            projection,
+            lifecycle,
+            left,
+            right,
+        } = self;
+        match lane_name.cmp(*label) {
+            Ordering::Less => left.lane_event(context, lane_name).map(Either::Left),
+            Ordering::Equal => {
+                let lane = projection(context);
+                let handler = lane.read_with_prev(|prev, map| {
+                    if let Some(ev) = prev {
+                        Some(map_handler(ev, lifecycle, map))
+                    } else {
+                        None
+                    }
+                });
+                handler.map(|h| Either::Right(Either::Left(h)))
+            }
+            Ordering::Greater => right
+                .lane_event(context, lane_name)
+                .map(|r| Either::Right(Either::Right(r))),
+        }
+    }
+}
+
+impl<'a, Context, Shared, K, V, LC, L, R> LaneEventShared<'a, Context, Shared>
+    for MapBranch<Context, K, V, LC, L, R>
+where
+    K: Clone + Eq + Hash,
+    LC: MapLaneLifecycleShared<K, V, Context, Shared>,
+    L: HTree + LaneEventShared<'a, Context, Shared>,
+    R: HTree + LaneEventShared<'a, Context, Shared>,
+{
+    type LaneEventHandler = MapBranchHandlerShared<'a, Context, Shared, K, V, LC, L, R>;
+
+    fn lane_event(
+        &'a self,
+        shared: &'a Shared,
+        handler_context: HandlerContext<Context>,
+        context: &Context,
+        lane_name: &str,
+    ) -> Option<Self::LaneEventHandler> {
+        let MapBranch {
+            label,
+            projection,
+            lifecycle,
+            left,
+            right,
+        } = self;
+        match lane_name.cmp(*label) {
+            Ordering::Less => left
+                .lane_event(shared, handler_context, context, lane_name)
+                .map(Either::Left),
+            Ordering::Equal => {
+                let lane = projection(context);
+                let handler = lane.read_with_prev(|prev, map| {
+                    if let Some(ev) = prev {
+                        Some(map_handler_shared(
+                            shared,
+                            handler_context,
+                            ev,
+                            lifecycle,
+                            map,
+                        ))
+                    } else {
+                        None
+                    }
+                });
+                handler.map(|h| Either::Right(Either::Left(h)))
             }
             Ordering::Greater => right
                 .lane_event(shared, handler_context, context, lane_name)
