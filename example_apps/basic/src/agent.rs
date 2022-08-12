7adf55e809e4d16bb77d076fa0d7f08ca24bb276
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2022-08-12 11:11:50 +0100

    Switched to swim crate.

--- example_apps/basic/src/agent.rs
+++ example_apps/basic/src/agent.rs
@@ -12,25 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use swim_agent::{lanes::{CommandLane, ValueLane}, AgentLaneModel, agent_lifecycle::utility::HandlerContext, event_handler::{EventHandler, HandlerActionExt}};
+use swim::agent::{AgentLaneModel, lanes::{ValueLane, CommandLane}, agent_lifecycle::utility::HandlerContext, event_handler::{EventHandler, HandlerActionExt}, projections, lifecycle};
 
 #[derive(AgentLaneModel)]
-#[swim_agent::projections]
+#[projections]
 pub struct ExampleAgent {
     length: ValueLane<i32>,
     update_value: CommandLane<String>,
 }
 
+#[derive(Clone)]
 pub struct ExampleLifecycle;
 
+#[lifecycle(ExampleAgent)]
 impl ExampleLifecycle {
 
+    #[on_start]
     pub fn on_start(&self, context: HandlerContext<ExampleAgent>) -> impl EventHandler<ExampleAgent> {
         context.get_agent_uri().and_then(move |uri| {
             context.effect(move || {
                 println!("Starting agent at: {}", uri);
             })
         })
+    }
+
+    #[on_command(update_value)]
+    pub fn on_update(&self, context: HandlerContext<ExampleAgent>, value: &String) -> impl EventHandler<ExampleAgent> {
+        let n = value.len() as i32;
+        context.set_value(ExampleAgent::LENGTH, n)
     }
 
 }
--- example_apps/basic/src/main.rs
+++ example_apps/basic/src/main.rs
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use swim::server::ServerBuilder;
+
 mod agent;
 
 fn main() {
+    
     println!("Hello, world!");
 }
