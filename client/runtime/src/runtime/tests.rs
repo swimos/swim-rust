e0bb601a87247b29b5c11f86181ef98752abda3d
SirCipher <tklapwijk92@gmail.com>
2023-01-16 18:23:05 +0000

    Moves client test fixture into its own crate

--- client/runtime/src/runtime/tests.rs
+++ client/runtime/src/runtime/tests.rs
@@ -12,200 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::borrow::BorrowMut;
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::future::{ready, Future};
-use std::io;
+use std::future::Future;
 use std::io::ErrorKind;
 use std::net::SocketAddr;
 use std::pin::Pin;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::task::{Context, Poll};
 use std::time::Duration;
 
 use bytes::BytesMut;
 use futures_util::future::{BoxFuture, Either};
-use futures_util::stream::Empty;
 use futures_util::FutureExt;
-use ratchet::{Message, NegotiatedExtension, NoExt, PayloadType, Role, WebSocket, WebSocketConfig};
-use tokio::io::{duplex, AsyncWriteExt, DuplexStream};
+use ratchet::{Message, NegotiatedExtension, NoExt, Role, WebSocket, WebSocketConfig};
+use tokio::io::{duplex, AsyncWriteExt};
 use tokio::sync::mpsc::unbounded_channel;
 use tokio::sync::{mpsc, oneshot, watch, Notify};
 use tokio::time::timeout;
 use tokio_util::codec::Encoder;
 use uuid::Uuid;
 
+use crate::runtime::error::DownlinkErrorKind;
+use crate::runtime::transport::TransportHandle;
+use crate::{start_runtime, DownlinkRuntimeError, RawHandle, Transport};
+use fixture::{MockExternalConnections, MockWs, Server, WsAction};
 use swim_api::downlink::{Downlink, DownlinkConfig, DownlinkKind};
 use swim_api::error::DownlinkTaskError;
 use swim_downlink::lifecycle::{BasicValueDownlinkLifecycle, ValueDownlinkLifecycle};
 use swim_downlink::{DownlinkTask, ValueDownlinkModel};
 use swim_model::address::{Address, RelativeAddress};
 use swim_model::path::{AbsolutePath, RelativePath};
-use swim_model::{Text, Value};
-use swim_recon::parser::{parse_recognize, Span};
-use swim_recon::printer::print_recon;
+use swim_model::Text;
 use swim_remote::AttachClient;
 use swim_runtime::compat::{RawRequestMessageEncoder, RequestMessage};
 use swim_runtime::downlink::{DownlinkOptions, DownlinkRuntimeConfig};
 use swim_runtime::error::{ConnectionError, IoError};
-use swim_runtime::remote::net::dns::{BoxDnsResolver, DnsResolver};
 use swim_runtime::remote::table::SchemeHostPort;
-use swim_runtime::remote::{ExternalConnections, Listener, Scheme, SchemeSocketAddr};
+use swim_runtime::remote::{Scheme, SchemeSocketAddr};
 use swim_runtime::routing::RoutingAddr;
-use swim_runtime::ws::{WsConnections, WsOpenFuture};
 use swim_utilities::algebra::non_zero_usize;
 use swim_utilities::io::byte_channel::{byte_channel, ByteReader, ByteWriter};
 use swim_utilities::trigger;
 use swim_utilities::trigger::{promise, Sender};
-use swim_warp::envelope::Envelope;
-
-use crate::runtime::error::DownlinkErrorKind;
-use crate::runtime::transport::TransportHandle;
-use crate::{start_runtime, DownlinkRuntimeError, RawHandle, Transport};
-
-#[derive(Debug)]
-struct Inner {
-    addrs: HashMap<SchemeHostPort, SchemeSocketAddr>,
-    sockets: HashMap<SocketAddr, DuplexStream>,
-}
-
-impl Inner {
-    fn new<R, S>(resolver: R, sockets: S) -> Inner
-    where
-        R: IntoIterator<Item = (SchemeHostPort, SchemeSocketAddr)>,
-        S: IntoIterator<Item = (SocketAddr, DuplexStream)>,
-    {
-        Inner {
-            addrs: HashMap::from_iter(resolver),
-            sockets: HashMap::from_iter(sockets),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-struct MockExternalConnections {
-    inner: Arc<Mutex<Inner>>,
-}
-
-impl MockExternalConnections {
-    fn new<R, S>(resolver: R, sockets: S) -> MockExternalConnections
-    where
-        R: IntoIterator<Item = (SchemeHostPort, SchemeSocketAddr)>,
-        S: IntoIterator<Item = (SocketAddr, DuplexStream)>,
-    {
-        MockExternalConnections {
-            inner: Arc::new(Mutex::new(Inner::new(resolver, sockets))),
-        }
-    }
-}
-
-struct MockListener;
-impl Listener for MockListener {
-    type Socket = DuplexStream;
-    type AcceptStream = Empty<io::Result<(DuplexStream, SchemeSocketAddr)>>;
-
-    fn into_stream(self) -> Self::AcceptStream {
-        panic!("Unexpected listener invocation")
-    }
-}
-
-impl ExternalConnections for MockExternalConnections {
-    type Socket = DuplexStream;
-    type ListenerType = MockListener;
-
-    fn bind(
-        &self,
-        _addr: SocketAddr,
-    ) -> BoxFuture<'static, io::Result<(SocketAddr, Self::ListenerType)>> {
-        panic!("Unexpected bind invocation")
-    }
-
-    fn try_open(&self, addr: SocketAddr) -> BoxFuture<'static, io::Result<Self::Socket>> {
-        let result = self
-            .inner
-            .lock()
-            .unwrap()
-            .sockets
-            .remove(&addr)
-            .ok_or(ErrorKind::NotFound.into());
-        ready(result).boxed()
-    }
-
-    fn dns_resolver(&self) -> BoxDnsResolver {
-        Box::new(self.clone())
-    }
-
-    fn lookup(
-        &self,
-        host: SchemeHostPort,
-    ) -> BoxFuture<'static, io::Result<Vec<SchemeSocketAddr>>> {
-        self.resolve(host).boxed()
-    }
-}
-
-impl DnsResolver for MockExternalConnections {
-    type ResolveFuture = BoxFuture<'static, io::Result<Vec<SchemeSocketAddr>>>;
-
-    fn resolve(&self, host: SchemeHostPort) -> Self::ResolveFuture {
-        let result = match self.inner.lock().unwrap().addrs.get(&host) {
-            Some(sock) => Ok(vec![sock.clone()]),
-            None => Err(io::ErrorKind::NotFound.into()),
-        };
-        ready(result).boxed()
-    }
-}
-
-enum WsAction {
-    Open,
-    Fail(ConnectionError),
-}
-
-struct MockWs {
-    states: HashMap<String, WsAction>,
-}
-
-impl MockWs {
-    fn new<S>(states: S) -> MockWs
-    where
-        S: IntoIterator<Item = (String, WsAction)>,
-    {
-        MockWs {
-            states: HashMap::from_iter(states),
-        }
-    }
-}
-
-impl WsConnections<DuplexStream> for MockWs {
-    type Ext = NoExt;
-    type Error = ConnectionError;
-
-    fn open_connection(
-        &self,
-        socket: DuplexStream,
-        addr: String,
-    ) -> WsOpenFuture<DuplexStream, Self::Ext, Self::Error> {
-        let result = match self.states.get(&addr) {
-            Some(WsAction::Open) => Ok(WebSocket::from_upgraded(
-                WebSocketConfig::default(),
-                socket,
-                NegotiatedExtension::from(NoExt),
-                BytesMut::default(),
-                Role::Client,
-            )),
-            Some(WsAction::Fail(e)) => Err(e.clone()),
-            None => Err(ConnectionError::Io(IoError::new(ErrorKind::NotFound, None))),
-        };
-        ready(result).boxed()
-    }
-
-    fn accept_connection(
-        &self,
-        _socket: DuplexStream,
-    ) -> WsOpenFuture<DuplexStream, Self::Ext, Self::Error> {
-        panic!("Unexpected accept connection invocation")
-    }
-}
 
 #[tokio::test]
 async fn transport_opens_connection_ok() {
@@ -219,7 +66,7 @@ async fn transport_opens_connection_ok() {
         )],
         [("127.0.0.1:9001".parse().unwrap(), client)],
     );
-    let ws = MockWs::new([("127.0.0.1".to_string(), WsAction::Open)]);
+    let ws = fixture::MockWs::new([("127.0.0.1".to_string(), WsAction::Open)]);
     let transport = Transport::new(ext, ws, non_zero_usize!(128));
 
     let (transport_tx, transport_rx) = mpsc::channel(128);
@@ -414,160 +261,6 @@ where
         .on_unlinked_blocking(|tx| {
             assert!(tx.send(TestMessage::Unlinked).is_ok());
         })
-}
-
-struct Lane<'l> {
-    node: String,
-    lane: String,
-    server: RefCell<&'l mut Server>,
-}
-
-impl<'l> Lane<'l> {
-    async fn read(&mut self) -> Envelope {
-        let Lane { server, .. } = self;
-        let Server { buf, transport } = server.get_mut();
-
-        match transport.borrow_mut().read(buf).await.unwrap() {
-            Message::Text => {}
-            m => panic!("Unexpected message type: {:?}", m),
-        }
-        let read = String::from_utf8(buf.to_vec()).unwrap();
-        buf.clear();
-
-        parse_recognize::<Envelope>(Span::new(&read), false).unwrap()
-    }
-
-    async fn write(&mut self, env: Envelope) {
-        let Lane { server, .. } = self;
-        let Server { transport, .. } = server.get_mut();
-
-        let response = print_recon(&env);
-        transport
-            .write(format!("{}", response), PayloadType::Text)
-            .await
-            .unwrap();
-    }
-
-    async fn await_link(&mut self) {
-        match self.read().await {
-            Envelope::Link {
-                node_uri, lane_uri, ..
-            } => {
-                assert_eq!(node_uri, self.node);
-                assert_eq!(lane_uri, self.lane);
-                self.write(Envelope::Linked {
-                    node_uri: node_uri.clone(),
-                    lane_uri: lane_uri.clone(),
-                    rate: None,
-                    prio: None,
-                    body: None,
-                })
-                .await;
-            }
-            e => panic!("Unexpected envelope {:?}", e),
-        }
-    }
-
-    async fn await_sync<V: Into<Value>>(&mut self, val: V) {
-        match self.read().await {
-            Envelope::Sync {
-                node_uri, lane_uri, ..
-            } => {
-                assert_eq!(node_uri, self.node);
-                assert_eq!(lane_uri, self.lane);
-                self.write(Envelope::Event {
-                    node_uri: node_uri.clone(),
-                    lane_uri: lane_uri.clone(),
-                    body: Some(val.into()),
-                })
-                .await;
-                self.write(Envelope::Synced {
-                    node_uri: node_uri.clone(),
-                    lane_uri: lane_uri.clone(),
-                    body: None,
-                })
-                .await;
-            }
-            e => panic!("Unexpected envelope {:?}", e),
-        }
-    }
-
-    async fn await_command(&mut self, expected: i32) {
-        match self.read().await {
-            Envelope::Command {
-                node_uri,
-                lane_uri,
-                body: Some(val),
-            } => {
-                assert_eq!(node_uri, self.node);
-                assert_eq!(lane_uri, self.lane);
-                assert_eq!(val, Value::Int32Value(expected));
-            }
-            e => panic!("Unexpected envelope {:?}", e),
-        }
-    }
-
-    async fn send_unlinked(&mut self) {
-        self.write(Envelope::Unlinked {
-            node_uri: self.node.clone().into(),
-            lane_uri: self.lane.clone().into(),
-            body: None,
-        })
-        .await;
-    }
-
-    async fn send_event<V: Into<Value>>(&mut self, val: V) {
-        self.write(Envelope::Event {
-            node_uri: self.node.clone().into(),
-            lane_uri: self.lane.clone().into(),
-            body: Some(val.into()),
-        })
-        .await;
-    }
-
-    async fn await_closed(&mut self) {
-        let Lane { server, .. } = self;
-        let Server { buf, transport } = server.get_mut();
-
-        match transport.borrow_mut().read(buf).await.unwrap() {
-            Message::Close(_) => {}
-            m => panic!("Unexpected message type: {:?}", m),
-        }
-    }
-}
-
-struct Server {
-    buf: BytesMut,
-    transport: WebSocket<DuplexStream, NoExt>,
-}
-
-impl Server {
-    fn lane_for<N, L>(&mut self, node: N, lane: L) -> Lane<'_>
-    where
-        N: ToString,
-        L: ToString,
-    {
-        Lane {
-            node: node.to_string(),
-            lane: lane.to_string(),
-            server: RefCell::new(self),
-        }
-    }
-}
-
-impl Server {
-    fn new(transport: DuplexStream) -> Server {
-        Server {
-            buf: BytesMut::new(),
-            transport: WebSocket::from_upgraded(
-                WebSocketConfig::default(),
-                transport,
-                NegotiatedExtension::from(NoExt),
-                BytesMut::default(),
-                Role::Server,
-            ),
-        }
-    }
 }
 
 struct DownlinkContext {
