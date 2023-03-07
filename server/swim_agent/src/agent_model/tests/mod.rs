4af3f514f6705e62d589ba33725880ab0dbf5087
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2023-03-07 12:36:06 +0000

    Updated copyright notice.

--- server/swim_agent/src/agent_model/tests/mod.rs
+++ server/swim_agent/src/agent_model/tests/mod.rs
@@ -1,6 +1,6 @@
 use std::{io::ErrorKind, sync::Arc};
 
-// Copyright 2015-2021 Swim Inc.
+// Copyright 2015-2023 Swim Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
