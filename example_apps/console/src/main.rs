1c47fa323ed2f57e8267fdc3c3ffe5e7f52217e4
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2023-03-20 12:06:43 +0000

    Added history view.

--- example_apps/console/src/main.rs
+++ example_apps/console/src/main.rs
@@ -12,10 +12,75 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use cursive::{views::{TextView, LinearLayout, Panel, EditView}, Cursive, view::{ScrollStrategy, Resizable, Nameable, Scrollable}, theme::{Theme, BorderStyle, Palette, BaseColor}, With, CursiveExt};
+use cursive::theme::Color::TerminalDefault;
+use cursive::theme::PaletteColor::*;
 mod controller;
 mod model;
 mod oneshot;
 mod runtime;
 mod shared_state;
+mod ui;
 
-fn main() {}
+fn main() {
+    let mut siv = Cursive::default();
+
+	siv.add_global_callback('q', |s| s.quit());
+
+    siv.set_theme(Theme {
+        shadow: false,
+        borders: BorderStyle::Outset,
+        palette: Palette::default().with(|palette| {
+            palette[Background] = TerminalDefault;
+            palette[View] = TerminalDefault;
+            palette[Primary] = BaseColor::White.dark();
+            palette[TitlePrimary] = BaseColor::Blue.light();
+            palette[Secondary] = BaseColor::White.dark();
+            palette[Highlight] = BaseColor::Blue.dark();
+        }),
+    });
+	siv.add_layer(
+        LinearLayout::horizontal()
+            .child(
+                LinearLayout::vertical()
+                    .child(
+                        Panel::new(
+                            EditView::new()
+                                .on_submit(on_command)
+                                .with_name("command")
+                                
+                        )
+                    )
+                    .child(
+                        Panel::new(
+                            TextView::new("")
+                                .with_name("history")
+                                .scrollable()
+                                .scroll_strategy(ScrollStrategy::StickToBottom)
+                        )
+                        .full_screen()
+                    )
+            )
+            .child(
+                Panel::new(TextView::new("World"))
+                    .full_screen()
+            )
+    );
+
+	siv.run();
+}
+
+fn on_command(cursive: &mut Cursive, text: &str) {
+    if text == "quit" {
+        cursive.quit();
+    }
+    cursive.call_on_name("history", |view: &mut TextView| {
+        view.append(format!("> {}\n", text))
+    });
+    
+    if let Some(cb) = cursive.call_on_name("command", |view: &mut EditView| {
+        view.set_content("")
+    }) {
+        cb(cursive);
+    }
+}
---
+++ example_apps/console/src/ui/history.rs
@@ -0,0 +1,260 @@
+// Copyright 2015-2023 Swim Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::{collections::VecDeque, rc::Rc, cell::RefCell};
+
+use cursive::{views::EditView, View, Vec2, Printer, event::{EventResult, AnyCb, Event, Key, Callback}, view::{Selector, ViewNotFound}, direction::Direction, Rect, Cursive};
+
+pub struct HistoryEditView {
+    inner: EditView,
+    history: History,
+}
+
+impl HistoryEditView {
+
+    pub fn new(max_history: usize) -> Self {
+        let history = History::new(max_history);
+
+        HistoryEditView { 
+            inner: EditView::new(),
+            history,
+        }
+    }
+
+    #[must_use]
+    pub fn on_submit<F>(mut self, callback: F) -> Self
+    where
+        F: Fn(&mut Cursive, &str) + 'static,
+    {
+        self.set_on_submit(callback);
+        self
+    }
+
+    pub fn set_on_submit<F>(&mut self, callback: F)
+    where
+        F: Fn(&mut Cursive, &str) + 'static,
+    {
+        let HistoryEditView { inner, .. } = self;
+        inner.set_on_submit(callback);
+    }
+
+    pub fn set_on_submit_mut<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut Cursive, &str) + 'static,
+    {
+        let HistoryEditView { inner, .. } = self;
+        inner.set_on_submit_mut(callback);
+    }
+
+    #[must_use]
+    pub fn on_submit_mut<F>(mut self, callback: F) -> Self
+    where
+        F: FnMut(&mut Cursive, &str) + 'static,
+    {
+        self.set_on_submit_mut(callback);
+        self
+    }
+
+    #[must_use]
+    pub fn on_edit<F>(mut self, callback: F) -> Self
+    where
+        F: Fn(&mut Cursive, &str, usize) + 'static,
+    {
+        self.set_on_edit(callback);
+        self
+    }
+
+    pub fn set_on_edit<F>(&mut self, callback: F)
+    where
+        F: Fn(&mut Cursive, &str, usize) + 'static,
+    {
+        let HistoryEditView { inner, .. } = self;
+        inner.set_on_edit(callback);
+    }
+
+    pub fn set_on_edit_mut<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut Cursive, &str, usize) + 'static,
+    {
+        let HistoryEditView { inner, .. } = self;
+        inner.set_on_edit_mut(callback);
+    }
+
+    #[must_use]
+    pub fn on_edit_mut<F>(mut self, callback: F) -> Self
+    where
+        F: FnMut(&mut Cursive, &str, usize) + 'static,
+    {
+        self.set_on_edit_mut(callback);
+        self
+    }
+
+    pub fn get_content(&self) -> Rc<String> {
+        self.inner.get_content()
+    }
+
+    pub fn set_content<S>(&mut self, content: S) -> Callback
+    where
+        S: Into<String>,
+    {
+        self.inner.set_content(content)
+    }
+
+    pub fn get_history(&self) -> &VecDeque<String> {
+        &self.history.entries
+    }
+
+}
+
+struct History {
+    max: usize,
+    entries: VecDeque<String>,
+    index: Option<usize>,
+}
+
+impl History {
+
+    fn new(max: usize) -> Self {
+        History { max, entries: VecDeque::new(), index: None }
+    }
+
+    fn push(&mut self, entry: String) {
+        let History { max, entries, index } = self;
+        entries.push_back(entry);
+        if entries.len() > *max {
+            entries.pop_front();
+        }
+        *index = None;
+    }
+
+    fn len(&self) -> usize {
+        self.entries.len()
+    }
+
+    fn get(&self) -> Option<&str> {
+        let History { entries, index, .. } = self;
+        (*index).and_then(|i| entries.get(i).map(String::as_str))
+    }
+
+    fn incr(&mut self) -> bool {
+        let History { entries, index, .. } = self;
+        match *index {
+            Some(i) => {
+                if i + 1 == entries.len() {
+                    false
+                } else {
+                    *index = Some(i + 1);
+                    true
+                }
+            },
+            None => {
+                if entries.is_empty() {
+                    false
+                } else {
+                    *index = Some(0);
+                    true
+                }
+            },
+        }
+    }
+
+    fn decr(&mut self) -> bool {
+        let History { index, .. } = self;
+        match *index {
+            Some(i) => {
+                if i == 0 {
+                    *index = None;
+                    true
+                } else {
+                    *index = Some(i - 1);
+                    true
+                }
+            },
+            None => false,
+        }
+    }
+
+}
+
+impl View for HistoryEditView {
+    fn draw(&self, printer: &Printer) {
+        self.inner.draw(printer)
+    }
+
+    fn layout(&mut self, dim: Vec2) {
+        self.inner.layout(dim)
+    }
+
+    fn needs_relayout(&self) -> bool {
+        self.inner.needs_relayout()
+    }
+
+    fn required_size(&mut self, constraint: Vec2) -> Vec2 {
+        self.inner.required_size(constraint)
+    }
+
+    fn on_event(&mut self, event: Event) -> EventResult {
+        match event {
+            Event::Key(Key::Up) => {
+                let HistoryEditView { inner, history } = self;
+                if history.incr() {
+                    if let Some(text) = history.get() {
+                        inner.set_content(text);
+                    }
+                }
+                EventResult::Consumed(None)
+            },
+            Event::Key(Key::Down) => {
+                let HistoryEditView { inner, history } = self;
+                if history.decr() {
+                    inner.set_content(history.get().unwrap_or(""));
+                }
+                EventResult::Consumed(None)
+            },
+            Event::Key(Key::Enter) => {
+                let HistoryEditView { inner, history } = self;
+                history.push((*inner.get_content()).clone());
+                let result = self.inner.on_event(Event::Key(Key::Enter));
+                self.inner.set_content("");
+                result.and(EventResult::Consumed(None))
+            }
+            ow => self.inner.on_event(ow),
+        }
+    }
+
+    fn call_on_any(&mut self, s: &Selector, cb: AnyCb) {
+        self.inner.call_on_any(s, cb)
+    }
+
+    fn focus_view(
+        &mut self,
+        s: &Selector,
+    ) -> Result<EventResult, ViewNotFound> {
+        self.inner.focus_view(s)
+    }
+
+    fn take_focus(
+        &mut self,
+        source: Direction,
+    ) -> Result<EventResult, cursive::view::CannotFocus> {
+        self.inner.take_focus(source)
+    }
+
+    fn important_area(&self, view_size: Vec2) -> Rect {
+        self.inner.important_area(view_size)
+    }
+
+}
+
+
