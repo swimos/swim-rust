c31f94ad748910a4a5bbfde4e2f7419e53a0f6a1
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2023-09-26 16:05:16 +0100

    Auto start agencies.

--- example_apps/transit/src/main.rs
+++ example_apps/transit/src/main.rs
@@ -12,11 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{error::Error, time::Duration};
+use std::{collections::HashSet, error::Error, time::Duration};
 
-use example_util::example_logging;
-use swim::{agent::agent_model::AgentModel, route::RoutePattern, server::ServerBuilder};
+use example_util::{example_logging, manage_handle};
+use futures::{stream::FuturesUnordered, StreamExt};
+use swim::{
+    agent::agent_model::AgentModel,
+    route::{RoutePattern, RouteUri},
+    server::{ServerBuilder, ServerHandle},
+};
 use tokio::time::Instant;
+use transit_model::agency::Agency;
 
 use crate::{
     agents::{
@@ -40,32 +46,16 @@ const HISTORY_LEN: usize = 10;
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     example_logging()?;
 
-    server_runner::run_server(|api: BusesApi| async move {
-        let agencies = model::agencies();
+    let agencies = model::agencies();
+    let agency_uris = agencies
+        .iter()
+        .map(|a| a.uri().parse::<RouteUri>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    server_runner::run_server(agency_uris, |api: BusesApi| async move {
         let mut builder = ServerBuilder::with_plane_name("Transit Plane");
 
-        for agency in agencies {
-            let uri = agency.uri();
-            let route = RoutePattern::parse_str(&uri)?;
-            let lifecycle = AgencyLifecycle::new(api.clone(), agency, POLL_DELAY);
-            let agent = AgentModel::new(AgencyAgent::default, lifecycle.into_lifecycle());
-            builder = builder.add_route(route, agent);
-        }
-
-        let epoch = Instant::now() - WEEK;
-        let vehicle_route = RoutePattern::parse_str("/vehicle/:country/:state/:id")?;
-        let vehicle_lifecycle = move || VehicleLifecycle::new(epoch, HISTORY_LEN).into_lifecycle();
-        let vehicle_agent = AgentModel::from_fn(VehicleAgent::default, vehicle_lifecycle);
-        builder = builder.add_route(vehicle_route, vehicle_agent);
-
-        let state_route = RoutePattern::parse_str("/state/:country/:state")?;
-        let state_agent = AgentModel::new(StateAgent::default, StateLifecycle.into_lifecycle());
-        builder = builder.add_route(state_route, state_agent);
-
-        let country_route = RoutePattern::parse_str("/country/:country")?;
-        let country_agent =
-            AgentModel::new(CountryAgent::default, CountryLifecycle.into_lifecycle());
-        builder = builder.add_route(country_route, country_agent);
+        builder = create_plane(agencies, api, builder, IncludeRoutes::all())?;
 
         let server = builder
             .update_config(|config| {
@@ -78,16 +68,91 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     .await
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum IncludeRoutes {
+    Vehicle,
+    State,
+    Country,
+}
+
+impl IncludeRoutes {
+    fn all() -> HashSet<Self> {
+        [
+            IncludeRoutes::Vehicle,
+            IncludeRoutes::State,
+            IncludeRoutes::Country,
+        ]
+        .into_iter()
+        .collect()
+    }
+}
+
+fn create_plane(
+    agencies: Vec<Agency>,
+    api: BusesApi,
+    mut builder: ServerBuilder,
+    inc: HashSet<IncludeRoutes>,
+) -> Result<ServerBuilder, Box<dyn Error + Send + Sync>> {
+    for agency in agencies {
+        let uri = agency.uri();
+        let route = RoutePattern::parse_str(&uri)?;
+        let lifecycle = AgencyLifecycle::new(api.clone(), agency, POLL_DELAY);
+        let agent = AgentModel::new(AgencyAgent::default, lifecycle.into_lifecycle());
+        builder = builder.add_route(route, agent);
+    }
+
+    if inc.contains(&IncludeRoutes::Vehicle) {
+        let epoch = Instant::now() - WEEK;
+        let vehicle_route = RoutePattern::parse_str("/vehicle/:country/:state/:id")?;
+        let vehicle_lifecycle = move || VehicleLifecycle::new(epoch, HISTORY_LEN).into_lifecycle();
+        let vehicle_agent = AgentModel::from_fn(VehicleAgent::default, vehicle_lifecycle);
+        builder = builder.add_route(vehicle_route, vehicle_agent);
+    }
+
+    if inc.contains(&IncludeRoutes::State) {
+        let state_route = RoutePattern::parse_str("/state/:country/:state")?;
+        let state_agent = AgentModel::new(StateAgent::default, StateLifecycle.into_lifecycle());
+        builder = builder.add_route(state_route, state_agent);
+    }
+
+    if inc.contains(&IncludeRoutes::Country) {
+        let country_route = RoutePattern::parse_str("/country/:country")?;
+        let country_agent =
+            AgentModel::new(CountryAgent::default, CountryLifecycle.into_lifecycle());
+        builder = builder.add_route(country_route, country_agent);
+    }
+
+    Ok(builder)
+}
+
+async fn start_agencies_and_wait(agency_uris: Vec<RouteUri>, handle: ServerHandle) {
+    let stream = agency_uris
+        .into_iter()
+        .map(|route| handle.start_agent(route))
+        .collect::<FuturesUnordered<_>>()
+        .filter_map(|r| async move { r.err() });
+    let errors = stream.collect::<Vec<_>>().await;
+    for error in errors {
+        println!("Failed to start agency agent: {}", error);
+    }
+    manage_handle(handle).await
+}
+
 #[cfg(feature = "mock-server")]
 mod server_runner {
-    use example_util::manage_handle;
     use std::{error::Error, future::Future, sync::Arc};
-    use swim::server::{BoxServer, Server};
+    use swim::{
+        route::RouteUri,
+        server::{BoxServer, Server},
+    };
     use tokio::{net::TcpListener, sync::Notify};
 
-    use crate::buses_api::BusesApi;
+    use crate::{buses_api::BusesApi, start_agencies_and_wait};
 
-    pub async fn run_server<F, Fut>(f: F) -> Result<(), Box<dyn Error + Send + Sync>>
+    pub async fn run_server<F, Fut>(
+        agency_uris: Vec<RouteUri>,
+        f: F,
+    ) -> Result<(), Box<dyn Error + Send + Sync>>
     where
         F: FnOnce(BusesApi) -> Fut,
         Fut: Future<Output = Result<BoxServer, Box<dyn Error + Send + Sync>>>,
@@ -113,7 +178,7 @@ mod server_runner {
             result
         };
 
-        let shutdown = manage_handle(handle);
+        let shutdown = start_agencies_and_wait(agency_uris, handle);
 
         let (_, mock_result, result) = tokio::join!(shutdown, mock_server, task_with_trigger);
 
@@ -125,12 +190,19 @@ mod server_runner {
 }
 #[cfg(not(feature = "mock-server"))]
 mod server_runner {
-    use example_util::manage_handle;
     use std::error::Error;
     use std::future::Future;
-    use swim::server::{BoxServer, Server};
+    use swim::{
+        route::RouteUri,
+        server::{BoxServer, Server},
+    };
 
-    pub async fn run_server<F, Fut>(f: F) -> Result<(), Box<dyn Error + Send + Sync>>
+    use crate::{buses_api::BusesApi, start_agencies_and_wait};
+
+    pub async fn run_server<F, Fut>(
+        agency_uris: Vec<RouteUri>,
+        f: F,
+    ) -> Result<(), Box<dyn Error + Send + Sync>>
     where
         F: FnOnce(BusesApi) -> Fut,
         Fut: Future<Output = Result<BoxServer, Box<dyn Error + Send + Sync>>>,
@@ -138,7 +210,7 @@ mod server_runner {
         let swim_server = f(BusesApi::default()).await?;
         let (task, handle) = swim_server.run();
 
-        let shutdown = manage_handle(handle);
+        let shutdown = start_agencies_and_wait(agency_uris, handle);
 
         let (_, result) = tokio::join!(shutdown, task);
 
