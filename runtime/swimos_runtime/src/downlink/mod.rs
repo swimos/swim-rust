289c37994f2edddd816d831b5acfe7e2b0377139
SirCipher <tklapwijk92@gmail.com>
2024-05-01 17:49:02 +0100

    Updates fix's documentation

--- runtime/swimos_runtime/src/downlink/mod.rs
+++ runtime/swimos_runtime/src/downlink/mod.rs
@@ -653,8 +653,8 @@ where
                     trace!("Entering Synced state.");
                     dl_state = ReadTaskDlState::Synced;
                     if is_active {
-                        // Current will be empty if we're communicating with a Supply Lane as no
-                        // event envelope will have been sent. Therefore, we can't use
+                        // `current` will be empty if we're communicating with a stateless lane as
+                        // no event envelope will have been sent. Therefore, we can't use
                         // `sync_current` as it will send an event notification with an empty body
                         // and cause the downlink's runtime task envelope decoder will potentially
                         // fail due to reading an extant. Therefore, delegate the operation to
