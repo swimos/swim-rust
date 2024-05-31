126b5e348392c0ff86aa85b0635a8a151ac76c26
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2024-05-31 14:11:35 +0100

    Top level documentation for the model crate.

--- api/swimos_model/src/lib.rs
+++ api/swimos_model/src/lib.rs
@@ -14,7 +14,41 @@
 
 //! # SwimOS Model
 //!
-//! Model types used by the SwimOS runtime and Swim applications.
+//! This model is a generic representation of the SwimOS serialization model. Any valid
+//! serialized record can be deserialized as an instance of the [`Value`] type in this
+//! crate.
+//!
+//! ## The Value type
+//!
+//! A SwimOS [`Value`] can be of any one of the following kinds.
+//!
+//! - An absent value (represented as [`Value::Extant`]).
+//! - A primitive value from the following set:
+//!  1. Booleans, represented as [`Value::BooleanValue`]
+//!  2. Integers. When deserializing, an appropriate Rust integer type will be chosen, depending on this size of the integer.
+//!  3. 64bit floating point numbers, represented as [`Value::Float64Value`].
+//!  4. UTF-8 Strings, represented as [`Value::Text`].
+//!  5. Arrays of bytes (represented as [`Value::Data`]).
+//! - A record consisting of an list of attributes ([`Attr`]) and  and list of items ([`Item`]). This is
+//! represented as [`Value::Record`].
+//!
+//! An attribute is a labelled (with a UTF-8 String) [`Value`].
+//!
+//! An item is one of:
+//!
+//! - A value item which is an instance of [`Value`]. This is represented as [`Item::ValueItem`].
+//! - A slot item which is a pair of two [`Value`]s, the first of which is interpreted as a key. This
+//! is represented as [`Item::Slot`].
+//!
+//! Note that the attributes and items of a record are always ordered, although the order may not always be
+//! significant, for example if a record, consisting only of slots, is used to represent a map.
+//!
+//! ## Using the model
+//!
+//! The use the model for serialization or deserialization, a format supporting the SwimOS model is required.
+//! The default format used by SwimsOS is the Recon markup language, an example of which can be found in the
+//! `swimos_recon` crate.
+//!
 
 /// Functions to define an identifier for the SwimOS model.
 pub mod identifier;
