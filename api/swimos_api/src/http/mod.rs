ea0bf062f5564a10d7942fe20decdfbd53abfb0c
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2024-05-31 13:45:01 +0100

    Added docs for addresses.

--- api/swimos_api/src/http/mod.rs
+++ api/swimos_api/src/http/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! A model of HTTP requests and responses.
+
 mod header;
 
 mod method;
--- api/swimos_api/src/lib.rs
+++ api/swimos_api/src/lib.rs
@@ -25,7 +25,5 @@
 pub mod agent;
 pub mod error;
 pub mod persistence;
-/// A model of HTTP requests and responses.
 pub mod http;
-/// Types to describe the addresses of local and remote lanes.
 pub mod address;
