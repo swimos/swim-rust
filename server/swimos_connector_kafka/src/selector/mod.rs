5c033277e7551d7d5518e33395c83e74b9850c53
Greg Holland <30577851+horned-sphere@users.noreply.github.com>
2024-07-17 16:22:21 +0100

    Selector parser.

--- server/swimos_connector_kafka/src/selector/mod.rs
+++ server/swimos_connector_kafka/src/selector/mod.rs
@@ -12,7 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::{num::ParseIntError, sync::OnceLock};
+
+use regex::Regex;
 use swimos_model::{Attr, Item, Text, Value};
+use thiserror::Error;
 
 use super::MessagePart;
 
@@ -245,4 +249,263 @@ impl Selector for Box<dyn Selector + Send> {
     }
 }
 
+static INIT_REGEX: OnceLock<Regex> = OnceLock::new();
+static FIELD_REGEX: OnceLock<Regex> = OnceLock::new();
 
+fn init_regex() -> &'static Regex {
+    INIT_REGEX.get_or_init(|| create_init_regex().expect("Invalid regex."))
+}
+
+fn field_regex() -> &'static Regex {
+    FIELD_REGEX.get_or_init(|| create_field_regex().expect("Invalid regex."))
+}
+
+fn create_init_regex() -> Result<Regex, regex::Error> {
+    Regex::new("\\A(\\$(?:key|value))(?:\\[(\\d+)])?\\z")
+}
+
+fn create_field_regex() -> Result<Regex, regex::Error> {
+    Regex::new("\\A(\\@?(?:\\w+))(?:\\[(\\d+)])?\\z")
+}
+
+pub struct SelectorComponent<'a> {
+    is_attr: bool,
+    name: &'a str,
+    index: Option<usize>,
+}
+
+pub struct SelectorDescriptor<'a> {
+    part: MessagePart,
+    index: Option<usize>,
+    components: Vec<SelectorComponent<'a>>,
+}
+
+impl<'a> SelectorDescriptor<'a> {
+
+    pub fn part(&self) -> MessagePart {
+        self.part
+    }
+
+    pub fn suggested_name(&self) -> Option<&'a str> {
+        let SelectorDescriptor { index, components, part } = self;
+        if let Some(SelectorComponent { name, index, .. }) = components.last() {
+            if index.is_none() {
+                Some(*name)
+            } else {
+                None
+            }
+        } else if index.is_none() {
+            Some(match part {
+                MessagePart::Key => "key",
+                MessagePart::Value => "value",
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn selector(&self) -> ChainSelector {
+        let SelectorDescriptor { index, components, .. } = self;
+        let mut links = vec![];
+        if let Some(n) = index {
+            links.push(BasicSelector::Index(IndexSelector::new(*n)));
+        }
+        for SelectorComponent { is_attr, name, index } in components {
+            links.push(if *is_attr {
+                BasicSelector::Attr(AttrSelector::new(name.to_string()))
+            } else {
+                BasicSelector::Slot(SlotSelector::for_field(*name))
+            });
+            if let Some(n) = index {
+                links.push(BasicSelector::Index(IndexSelector::new(*n)));
+            }
+        }
+        ChainSelector::new(links)
+    }
+
+}
+
+#[derive(Clone, Copy, Error, Debug)]
+pub enum BadSelector {
+    #[error("Selector strings cannot be empty.")]
+    EmptySelector,
+    #[error("Selector components cannot be empty.")]
+    EmptyComponent,
+    #[error("Invalid root selector (must be one of '$key' or '$value' with an optional index).")]
+    InvalidRoot,
+    #[error("Invalid component selector (must be an attribute or slot name with an optional index).")]
+    InvalidComponent,
+    #[error("An index specified was not a valid usize.")]
+    IndexOutOfRange,
+}
+
+impl From<ParseIntError> for BadSelector {
+    fn from(_value: ParseIntError) -> Self {
+        BadSelector::IndexOutOfRange
+    }
+}
+
+pub fn parse_selector(descriptor: &str) -> Result<SelectorDescriptor<'_>, BadSelector> {
+    if (descriptor.is_empty()) {
+        return Err(BadSelector::EmptySelector)
+    }
+    let mut it = descriptor.split('.');
+    let (part, index) = match it.next() {
+        Some(root) if !root.is_empty() => {
+            if let Some(captures) = init_regex().captures(root) {
+                let part = match captures.get(1) {
+                    Some(kind) if kind.as_str() == "$key" => MessagePart::Key,
+                    Some(kind) if kind.as_str() == "$value" => MessagePart::Value,
+                    _ => return Err(BadSelector::InvalidRoot),
+                };
+                let index = if let Some(index_match) = captures.get(2) {
+                    Some(index_match.as_str().parse::<usize>()?)
+                } else {
+                    None
+                };
+                (part, index)
+            } else {
+                return Err(BadSelector::InvalidRoot);
+            }
+        }
+        _ => return Err(BadSelector::EmptyComponent),
+    };
+
+    let mut components = vec![];
+    for part in it {
+        if part.is_empty() {
+            return Err(BadSelector::EmptyComponent);
+        }
+        if let Some(captures) = field_regex().captures(part) {
+            let (is_attr, name) = match captures.get(1) {
+                Some(name) if name.as_str().starts_with('@') => (true, &name.as_str()[1..]),
+                Some(name) => (false, name.as_str()),
+                _ => return Err(BadSelector::InvalidComponent),
+            };
+            let index = if let Some(index_match) = captures.get(2) {
+                Some(index_match.as_str().parse::<usize>()?)
+            } else {
+                None
+            };
+            components.push(SelectorComponent { is_attr, name, index });
+        } else {
+            return Err(BadSelector::InvalidRoot);
+        }
+    }
+    
+    Ok(SelectorDescriptor { part, index, components })
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn init_regex_creation() {
+        super::create_init_regex().expect("Creation failed.");
+    }
+
+    #[test]
+    fn match_key() {
+        if let Some(captures) = super::init_regex().captures("$key") {
+            let kind = captures.get(1).expect("Missing capture.");
+            assert!(captures.get(2).is_none());
+            assert_eq!(kind.as_str(), "$key");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_value() {
+        if let Some(captures) = super::init_regex().captures("$value") {
+            let kind = captures.get(1).expect("Missing capture.");
+            assert!(captures.get(2).is_none());
+            assert_eq!(kind.as_str(), "$value");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_key_indexed() {
+        if let Some(captures) = super::init_regex().captures("$key[3]") {
+            let kind = captures.get(1).expect("Missing capture.");
+            let index = captures.get(2).expect("Missing capture.");
+            assert_eq!(kind.as_str(), "$key");
+            assert_eq!(index.as_str(), "3");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_value_indexed() {
+        if let Some(captures) = super::init_regex().captures("$value[0]") {
+            let kind = captures.get(1).expect("Missing capture.");
+            let index = captures.get(2).expect("Missing capture.");
+            assert_eq!(kind.as_str(), "$value");
+            assert_eq!(index.as_str(), "0");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_attr() {
+        if let Some(captures) = super::field_regex().captures("@my_attr") {
+            let name = captures.get(1).expect("Missing capture.");
+            assert!(captures.get(2).is_none());
+            assert_eq!(name.as_str(), "@my_attr");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_attr_indexed() {
+        if let Some(captures) = super::field_regex().captures("@attr[73]") {
+            let name = captures.get(1).expect("Missing capture.");
+            let index = captures.get(2).expect("Missing capture.");
+            assert_eq!(name.as_str(), "@attr");
+            assert_eq!(index.as_str(), "73");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_slot() {
+        if let Some(captures) = super::field_regex().captures("slot") {
+            let name = captures.get(1).expect("Missing capture.");
+            assert!(captures.get(2).is_none());
+            assert_eq!(name.as_str(), "slot");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_slot_indexed() {
+        if let Some(captures) = super::field_regex().captures("slot5[123]") {
+            let name = captures.get(1).expect("Missing capture.");
+            let index = captures.get(2).expect("Missing capture.");
+            assert_eq!(name.as_str(), "slot5");
+            assert_eq!(index.as_str(), "123");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+    #[test]
+    fn match_slot_non_latin() {
+        if let Some(captures) = super::field_regex().captures("اسم[123]") {
+            let name = captures.get(1).expect("Missing capture.");
+            let index = captures.get(2).expect("Missing capture.");
+            assert_eq!(name.as_str(), "اسم");
+            assert_eq!(index.as_str(), "123");
+        } else {
+            panic!("Did not match.");
+        }
+    }
+
+}
